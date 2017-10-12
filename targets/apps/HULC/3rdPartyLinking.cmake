7c5995c90c6f143a3cb4f79011463ba1ef93756b
Soeren Peters <peters@irmb.tu-bs.de>
2017-10-12 17:07:13 +0200

    - add JsonCpp option

--- targets/apps/HULC/3rdPartyLinking.cmake
+++ targets/apps/HULC/3rdPartyLinking.cmake
@@ -2,3 +2,8 @@ include (${CMAKE_SOURCE_DIR}/${cmakeMacroPath}/MPI/Link.cmake)
 linkMPI(${targetName})
 include (${CMAKE_SOURCE_DIR}/${cmakeMacroPath}/Cuda/Link.cmake)
 linkCuda(${targetName})
+
+if(HULC.BUILD_JSONCPP)
+  include (${CMAKE_SOURCE_DIR}/${cmakeMacroPath}/JsonCpp/Link.cmake)
+  linkJsonCpp(${targetName})
+endif()
