1c489930f5506ee9c49910c97c75606261f91f3d
kutscher <kutscher@irmb.tu-bs.de>
2018-01-02 13:14:13 +0100

    library is changed

--- source/ThirdParty/Library/numerics/geometry3d/CoordinateTransformation3D.h
+++ source/ThirdParty/Library/numerics/geometry3d/CoordinateTransformation3D.h
@@ -154,9 +154,10 @@ private:
    bool   active;
    bool   transformation;
 
-   friend class MPIIORestartCoProcessor;
+   friend class MPIIORestart1CoProcessor;
    friend class MPIIORestart2CoProcessor;
-   friend class MPIIORestart3CoProcessor;
+   friend class MPIIORestart11CoProcessor;
+   friend class MPIIORestart21CoProcessor;
 
    friend class boost::serialization::access;
    template<class Archive>
