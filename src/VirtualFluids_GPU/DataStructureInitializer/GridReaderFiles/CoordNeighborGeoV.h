7d5dc786f6156d6b5dd7fca0b85664ba7df0b8b6
Soeren Peters <peters@irmb.tu-bs.de>
2018-01-10 13:11:31 +0100

    - adds DataStructureInitalizer folder with new GridProvider

---
+++ src/VirtualFluids_GPU/DataStructureInitializer/GridReaderFiles/CoordNeighborGeoV.h
@@ -0,0 +1,45 @@
+#ifndef CoordNeighborGeoV_H
+#define CoordNeighborGeoV_H
+
+#include <vector>
+#include <string>
+#include <fstream>
+#include "LBM/LB.h"
+
+
+class CoordNeighborGeoV 
+{
+protected:
+	std::ifstream file; 
+	unsigned int maxLevel; 
+	std::vector<unsigned int> levelSizes;
+	std::vector< std::vector<unsigned int> > neighbors;
+	std::vector< std::vector< doubflo> > coordinates; 
+
+public:
+	CoordNeighborGeoV();
+	CoordNeighborGeoV(std::string ad, bool binaer, bool coord);
+	~CoordNeighborGeoV(void);
+
+	void init(bool coord);
+	void init_Binary(bool coord);
+
+	unsigned int getLevel();
+	unsigned int getSize(unsigned int level);
+	std::vector<unsigned int>getVec(unsigned int level);
+	void setVec(unsigned int level, std::vector<unsigned int> vec);
+
+	void initalNeighbors(unsigned int *int_ptr, unsigned int level ) const;
+	void initalCoords(doubflo *int_ptr, unsigned int level ) const;
+
+private:
+	void skipSpace();
+	void readLevelSize(unsigned int level);
+	void readNeighbors(unsigned int level);
+	void readCoordinates(unsigned int level);
+	void resizeVectors();
+	void readLevel();
+
+};
+
+#endif
