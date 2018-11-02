a57d471ddbac7d2cb3c9435480522e0ae8ebe82b
Timon Habenicht <t.habenicht@tu-bs.de>
2018-11-02 11:07:24 +0100

    adds Kernel and KernelFactory to VirtualFluids

---
+++ src/VirtualFluids_GPU/Kernel/KernelFactory.h
@@ -0,0 +1,26 @@
+#ifndef KERNEL_FACTORY_H
+#define KERNEL_FACTORY_H
+
+#include <memory>
+#include <vector>
+#include <iostream>
+
+class Kernel;
+class Parameter;
+
+class KernelFactory
+{
+public:
+	static std::shared_ptr< KernelFactory> getNewInstance(std::shared_ptr<Parameter> para);
+	std::vector< std::shared_ptr< Kernel>> makeKernels(int maxLevel, std::string kernelName);
+	void setKernelAtLevel(std::vector< std::shared_ptr<Kernel>> kernels, int i, std::string kernelName);
+
+private:
+	std::shared_ptr< Kernel> makeKernel(std::string kernelName, int level);
+	KernelFactory(std::shared_ptr<Parameter> para);
+	KernelFactory();
+
+	std::vector< std::shared_ptr< Kernel>> kernels;
+	std::shared_ptr< Parameter> para;
+};
+#endif
