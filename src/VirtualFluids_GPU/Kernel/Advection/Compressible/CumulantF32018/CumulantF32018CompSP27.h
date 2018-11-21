d688c76da91a315c6a32df1e820d28a732c187b1
Timon Habenicht <t.habenicht@tu-bs.de>
2018-11-21 11:04:43 +0100

    adds Kernel CumulantF32018

---
+++ src/VirtualFluids_GPU/Kernel/Advection/Compressible/CumulantF32018/CumulantF32018CompSP27.h
@@ -0,0 +1,22 @@
+#ifndef CUMULANT_F32018_COMP_SP27_H
+#define CUMULANT_F32018_COMP_SP27_H
+
+#include "Kernel\Kernel.h"
+
+#include <memory>
+
+class CumulantF32018CompSP27 : public Kernel
+{
+public:
+	static std::shared_ptr< Kernel> getNewInstance(std::shared_ptr< Parameter> para, int level);
+	void run();
+	bool checkParameter();
+
+private:
+	CumulantF32018CompSP27(std::shared_ptr< Parameter> para, int level);
+	std::shared_ptr< Parameter> para;
+	int level;
+
+};
+
+#endif 
