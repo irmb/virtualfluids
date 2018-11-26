798ae9fabc617574e9c9f6b08a566bbab70b5694
LEGOLAS\lenz <lenz@irmb.tu-bs.de>
2018-11-26 17:58:05 +0100

    implements pressure boundary condition

---
+++ src/GksGpu/BoundaryConditions/Pressure.cu
@@ -0,0 +1,147 @@
+#include "Pressure.h"
+
+#define _USE_MATH_DEFINES
+#include <math.h>
+
+#include <cuda.h>
+#include <cuda_runtime.h>
+#include <helper_cuda.h>
+
+#include "Core/PointerDefinitions.h"
+#include "Core/RealConstants.h"
+
+#include "DataBase/DataBase.h"
+#include "DataBase/DataBaseStruct.h"
+
+#include "Definitions/MemoryAccessPattern.h"
+#include "Definitions/PassiveScalar.h"
+
+#include "FlowStateData/FlowStateData.cuh"
+#include "FlowstateData/AccessDeviceData.cuh"
+
+#include "CudaUtility/CudaRunKernel.hpp"
+
+//////////////////////////////////////////////////////////////////////////
+
+__global__                 void boundaryConditionKernel  ( const DataBaseStruct dataBase, 
+                                                           const PressureStruct boundaryCondition, 
+                                                           const Parameters parameters,
+                                                           const uint startIndex,
+                                                           const uint numberOfEntities );
+
+__host__ __device__ inline void boundaryConditionFunction( const DataBaseStruct& dataBase, 
+                                                           const PressureStruct& boundaryCondition, 
+                                                           const Parameters& parameters,
+                                                           const uint startIndex,
+                                                           const uint index );
+
+//////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////
+
+void Pressure::runBoundaryConditionKernel(const SPtr<DataBase> dataBase, 
+                                                const Parameters parameters, 
+                                                const uint level)
+{    
+    CudaUtility::CudaGrid grid( this->numberOfCellsPerLevel[ level ], 32 );
+
+    runKernel( boundaryConditionKernel,
+               boundaryConditionFunction,
+               dataBase->getDeviceType(), grid, 
+               dataBase->toStruct(),
+               this->toStruct(),
+               parameters,
+               this->startOfCellsPerLevel[ level ] );
+
+    getLastCudaError("IsothermalWall::runBoundaryConditionKernel( const SPtr<DataBase> dataBase, const Parameters parameters, const uint level )");
+}
+
+//////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////
+
+__global__ void boundaryConditionKernel(const DataBaseStruct dataBase, 
+                                        const PressureStruct boundaryCondition, 
+                                        const Parameters parameters,
+                                        const uint startIndex,
+                                        const uint numberOfEntities)
+{
+    uint index = blockIdx.x * blockDim.x + threadIdx.x;
+
+    if( index >= numberOfEntities ) return;
+
+    boundaryConditionFunction( dataBase, boundaryCondition, parameters, startIndex, index );
+}
+
+__host__ __device__ inline void boundaryConditionFunction(const DataBaseStruct& dataBase, 
+                                                          const PressureStruct& boundaryCondition, 
+                                                          const Parameters& parameters,
+                                                          const uint startIndex,
+                                                          const uint index)
+{
+    uint ghostCellIdx  = boundaryCondition.ghostCells [ startIndex + index ];
+    uint domainCellIdx = boundaryCondition.domainCells[ startIndex + index ];
+    uint secondCellIdx = boundaryCondition.secondCells[ startIndex + index ];
+
+    PrimitiveVariables ghostCellPrim;
+    {
+        PrimitiveVariables domainCellPrim;
+        PrimitiveVariables secondCellPrim;
+
+        {
+            ConservedVariables domainCellData;
+            readCellData( domainCellIdx, dataBase, domainCellData );
+            domainCellPrim = toPrimitiveVariables( domainCellData, parameters.K );
+
+            ConservedVariables secondCellData;
+            if( secondCellIdx != INVALID_INDEX ){
+                readCellData( secondCellIdx, dataBase, secondCellData );
+                secondCellPrim = toPrimitiveVariables( secondCellData, parameters.K );
+            }
+        }
+
+    //    ghostCellPrim.U      = two * domainCellPrim.U      - secondCellPrim.U;
+    //    ghostCellPrim.V      = two * domainCellPrim.V      - secondCellPrim.V;
+    //    ghostCellPrim.W      = two * domainCellPrim.W      - secondCellPrim.W;
+    //    ghostCellPrim.lambda = two * domainCellPrim.lambda - secondCellPrim.lambda;
+    //#ifdef USE_PASSIVE_SCALAR
+    //    ghostCellPrim.S      = two * domainCellPrim.S      - secondCellPrim.S;
+    //#endif // USE_PASSIVE_SCALAR
+
+        ghostCellPrim.U      = domainCellPrim.U     ;
+        ghostCellPrim.V      = domainCellPrim.V     ;
+        ghostCellPrim.W      = domainCellPrim.W     ;
+        ghostCellPrim.lambda = domainCellPrim.lambda;
+    #ifdef USE_PASSIVE_SCALAR
+        ghostCellPrim.S      = domainCellPrim.S     ;
+    #endif // USE_PASSIVE_SCALAR
+
+
+        real rho0 = ( two * boundaryCondition.p0 * c1o2 * ( domainCellPrim.lambda + ghostCellPrim.lambda ) );
+
+        ghostCellPrim.rho = two * rho0 - domainCellPrim.rho;
+    }
+
+    {
+        ConservedVariables ghostCons = toConservedVariables( ghostCellPrim, parameters.K );
+
+        writeCellData( ghostCellIdx, dataBase, ghostCons );
+    }
+}
+
+Pressure::Pressure(SPtr<DataBase> dataBase, real p0)
+    : BoundaryCondition( dataBase )
+{
+    this->p0 = p0;
+}
+
+bool Pressure::isWall()
+{
+    return false;
+}
+
+bool Pressure::secondCellsNeeded()
+{
+    return true;
+}
+
---
+++ src/GksGpu/BoundaryConditions/Pressure.h
@@ -0,0 +1,62 @@
+#ifndef Pressure_CUH
+#define Pressure_CUH
+
+#include <memory>
+
+#include "VirtualFluidsDefinitions.h"
+
+#include "Core/PointerDefinitions.h"
+#include "Core/DataTypes.h"
+#include "Core/VectorTypes.h"
+
+#include "BoundaryConditions/BoundaryCondition.h"
+
+//struct IsothermalWallStruct : virtual public BoundaryConditionStruct
+//{
+//    Vec3 velocity;
+//    real lambda;
+//    real S;
+//};
+
+struct PressureStruct
+{
+    uint  numberOfCells;
+
+    uint* ghostCells;
+    uint* domainCells;
+    uint* secondCells;
+
+    real p0;
+};
+
+struct VF_PUBLIC Pressure : public BoundaryCondition
+{
+    real p0;
+
+    Pressure( SPtr<DataBase> dataBase, real p0 );
+
+    virtual bool isWall() override;
+
+    virtual bool secondCellsNeeded() override;
+
+    virtual void runBoundaryConditionKernel(const SPtr<DataBase> dataBase,
+                                            const Parameters parameters, 
+                                            const uint level) override;
+
+    PressureStruct toStruct()
+    {
+        PressureStruct boundaryCondition;
+
+        boundaryCondition.numberOfCells = this->numberOfCells;
+
+        boundaryCondition.ghostCells    = this->ghostCells;
+        boundaryCondition.domainCells   = this->domainCells;
+        boundaryCondition.secondCells   = this->secondCells;
+
+        boundaryCondition.p0            = this->p0;
+
+        return boundaryCondition;
+    }
+};
+
+#endif
