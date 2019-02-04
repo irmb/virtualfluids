55b7e28b79b37a575f34120ddb5f46a74b9aee75
Shina Maini <maini.shina9393@gmail.com>
2019-02-04 15:08:16 +0100

    added IntegratorHelper for location of observation point

--- source/Applications/OrganPipe/OrganPipe.cpp
+++ source/Applications/OrganPipe/OrganPipe.cpp
@@ -18,6 +18,8 @@ void run()
    int refineLevel = 9;
    double deltaXfine = deltaXcoarse/(double)(1 << refineLevel);
 
+   double availMem = 5e9;
+
    string pathOut = "e:/temp/OrganPipe";
    string pathGeo = "C:/Users/maini/Desktop/organflute/02_organf_scaled.stl";
 
@@ -117,14 +119,14 @@ void run()
    if (myid == 0) GbSystem3D::writeGeoObject(refineSphereL5.get(), pathOut + "/geo/refineSphereL5", WbWriterVtkXmlASCII::getInstance());
 
    //full pipe refine
-   SPtr<GbObject3D> refineBoxL7(new GbCuboid3D(-0.1107, -0.0165, -0.0165, 0.1317, 0.0165, 0.0165));
+   SPtr<GbObject3D> refineBoxL7(new GbCuboid3D(-0.1132, -0.019, -0.019, 0.1342, 0.019, 0.019));
    if (myid == 0) GbSystem3D::writeGeoObject(refineBoxL7.get(), pathOut + "/geo/refineBoxL7", WbWriterVtkXmlASCII::getInstance());
 
    //refine languid and upperlip
-   SPtr<GbObject3D> refineBoxL9(new GbCuboid3D(-0.1007, -0.0165, -0.0165, -0.0627, 0.0165, 0.0165));
+   SPtr<GbObject3D> refineBoxL9(new GbCuboid3D(-0.1007, -0.0115, -0.0115, -0.0634, 0.0115, 0.01255));
    if (myid == 0) GbSystem3D::writeGeoObject(refineBoxL9.get(), pathOut + "/geo/refineBoxL9", WbWriterVtkXmlASCII::getInstance());
   
-   SPtr<GbSphere3D> refineSphereL9(new GbSphere3D(g_minX1+75e-3, 0.0, 0.015, 24e-3));
+   SPtr<GbSphere3D> refineSphereL9(new GbSphere3D(g_minX1+75e-3, 0.0, 0.015, 13e-3));
    if (myid == 0) GbSystem3D::writeGeoObject(refineSphereL9.get(), pathOut + "/geo/refineSphereL9", WbWriterVtkXmlASCII::getInstance());
 
    if (refineLevel > 0)
@@ -174,6 +176,162 @@ void run()
    SPtr<D3Q27Interactor> inflowIntr = SPtr<D3Q27Interactor>(new D3Q27Interactor(geoInflow, grid, velBCAdapter, Interactor3D::SOLID));
    //outflow
    SPtr<D3Q27Interactor> outflowIntr = SPtr<D3Q27Interactor>(new D3Q27Interactor(geoOutflow, grid, outflowBCAdapter, Interactor3D::SOLID));
+
+   ////////////////////////////////////////////
+   //METIS
+   SPtr<Grid3DVisitor> metisVisitor(new MetisPartitioningGridVisitor(comm, MetisPartitioningGridVisitor::LevelBased, D3Q27System::BSW, MetisPartitioner::KWAY));
+   ////////////////////////////////////////////
+   /////delete solid blocks
+   if (myid == 0) UBLOG(logINFO, "deleteSolidBlocks - start");
+   InteractorsHelper intHelper(grid, metisVisitor);
+   intHelper.addInteractor(inflowIntr);
+   intHelper.addInteractor(outflowIntr);
+   intHelper.addInteractor(addWallZminInt);
+   intHelper.addInteractor(addWallZmaxInt);
+   intHelper.addInteractor(addWallYminInt);
+   intHelper.addInteractor(addWallYmaxInt);
+   intHelper.addInteractor(opipeInter);
+   intHelper.selectBlocks();
+   if (myid == 0) UBLOG(logINFO, "deleteSolidBlocks - end");
+
+   {
+      WriteBlocksCoProcessor ppblocks(grid, SPtr<UbScheduler>(new UbScheduler(1)), pathOut, WbWriterVtkXmlBinary::getInstance(), comm);
+      ppblocks.process(1);
+   }
+
+   unsigned long long numberOfBlocks = (unsigned long long)grid->getNumberOfBlocks();
+   int ghostLayer = 3;
+   unsigned long long numberOfNodesPerBlock = (unsigned long long)(blocknx[0])* (unsigned long long)(blocknx[1])* (unsigned long long)(blocknx[2]);
+   unsigned long long numberOfNodes = numberOfBlocks * numberOfNodesPerBlock;
+   unsigned long long numberOfNodesPerBlockWithGhostLayer = numberOfBlocks * (blocknx[0] + ghostLayer) * (blocknx[1] + ghostLayer) * (blocknx[2] + ghostLayer);
+   double needMemAll = double(numberOfNodesPerBlockWithGhostLayer*(27 * sizeof(double) + sizeof(int) + sizeof(float) * 4));
+   double needMem = needMemAll / double(comm->getNumberOfProcesses());
+
+   if (myid == 0)
+   {
+      UBLOG(logINFO, "Number of blocks = " << numberOfBlocks);
+      UBLOG(logINFO, "Number of nodes  = " << numberOfNodes);
+      int minInitLevel = grid->getCoarsestInitializedLevel();
+      int maxInitLevel = grid->getFinestInitializedLevel();
+      for (int level = minInitLevel; level <= maxInitLevel; level++)
+      {
+         int nobl = grid->getNumberOfBlocks(level);
+         UBLOG(logINFO, "Number of blocks for level " << level << " = " << nobl);
+         UBLOG(logINFO, "Number of nodes for level " << level << " = " << nobl*numberOfNodesPerBlock);
+      }
+      UBLOG(logINFO, "Necessary memory  = " << needMemAll << " bytes");
+      UBLOG(logINFO, "Necessary memory per process = " << needMem << " bytes");
+      UBLOG(logINFO, "Available memory per process = " << availMem << " bytes");
+   }
+
+   SetKernelBlockVisitor kernelVisitor(kernel, nu_LB, availMem, needMem);
+   grid->accept(kernelVisitor);
+
+   if (myid == 0) UBLOG(logINFO, "SetKernelBlockVisitor:end");
+
+   if (myid == 0)
+   {
+      UBLOG(logINFO, "PID = " << myid << " Point 5");
+      UBLOG(logINFO, "PID = " << myid << " Physical Memory currently used by current process: " << Utilities::getPhysMemUsedByMe() / 1073741824.0 << " GB");
+   }
+
+   if (refineLevel > 0)
+   {
+      SetUndefinedNodesBlockVisitor undefNodesVisitor;
+      grid->accept(undefNodesVisitor);
+   }
+
+   if (myid == 0) UBLOG(logINFO, "SetUndefinedNodesBlockVisitor:end");
+
+   //BC
+   intHelper.setBC();
+   if (myid == 0) UBLOG(logINFO, "intHelper.setBC():end");
+
+   //initialization of distributions
+   InitDistributionsBlockVisitor initVisitor1;
+   //initVisitor1.setVx1(fct);
+   grid->accept(initVisitor1);
+
+   ////set connectors
+   InterpolationProcessorPtr iProcessor(new CompressibleOffsetInterpolationProcessor());
+   SetConnectorsBlockVisitor setConnsVisitor(comm, true, D3Q27System::ENDDIR, nu_LB, iProcessor);
+   grid->accept(setConnsVisitor);
+
+   //bcVisitor should be accept after initialization!!!!
+   grid->accept(bcVisitor);
+   if (myid == 0) UBLOG(logINFO, "grid->accept(bcVisitor):end");
+
+   //Post process
+   {
+      SPtr<UbScheduler> geoSch(new UbScheduler(1));
+      WriteBoundaryConditionsCoProcessor ppgeo(grid, geoSch, pathOut, WbWriterVtkXmlBinary::getInstance(), comm);
+      ppgeo.process(0);
+   }
+
+   ////sponge layer
+   ////////////////////////////////////////////////////////////////////////////
+   /*GbCuboid3DPtr spongeLayerX1max(new GbCuboid3D(g_maxX1 - 0.4, g_minX2 - blockLength, g_minX3 - blockLength, g_maxX1 + blockLength, g_maxX2 + blockLength, g_maxX3 + blockLength));
+   if (myid == 0) GbSystem3D::writeGeoObject(spongeLayerX1max.get(), pathOut + "/geo/spongeLayerX1max", WbWriterVtkXmlASCII::getInstance());
+   SpongeLayerBlockVisitor slVisitorX1max(spongeLayerX1max, kernel, nu_LB, D3Q27System::E);
+   grid->accept(slVisitorX1max);
+
+   GbCuboid3DPtr spongeLayerX1min(new GbCuboid3D(g_minX1 - blockLength, g_minX2 - blockLength, g_minX3 - blockLength, g_minX1 + 0.2, g_maxX2 + blockLength, g_maxX3 + blockLength));
+   if (myid == 0) GbSystem3D::writeGeoObject(spongeLayerX1min.get(), pathOut + "/geo/spongeLayerX1min", WbWriterVtkXmlASCII::getInstance());
+   SpongeLayerBlockVisitor slVisitorX1min(spongeLayerX1min, spKernel, nuLB, D3Q27System::W);
+   grid->accept(slVisitorX1min);
+
+   GbCuboid3DPtr spongeLayerX2max(new GbCuboid3D(g_maxX1 - 0.4, g_minX2 - blockLength, g_minX3 - blockLength, g_maxX1 + blockLength, g_maxX2 + blockLength, g_maxX3 + blockLength));
+   if (myid == 0) GbSystem3D::writeGeoObject(spongeLayerX1max.get(), pathOut + "/geo/spongeLayerX1max", WbWriterVtkXmlASCII::getInstance());
+   SpongeLayerBlockVisitor slVisitorX1max(spongeLayerX1max, kernel, nu_LB, D3Q27System::E);
+   grid->accept(slVisitorX1max);
+
+   GbCuboid3DPtr spongeLayerX2min(new GbCuboid3D(g_minX1 - blockLength, g_minX2 - blockLength, g_minX3 - blockLength, g_minX1 + 0.2, g_maxX2 + blockLength, g_maxX3 + blockLength));
+   if (myid == 0) GbSystem3D::writeGeoObject(spongeLayerX1min.get(), pathOut + "/geo/spongeLayerX1min", WbWriterVtkXmlASCII::getInstance());
+   SpongeLayerBlockVisitor slVisitorX1min(spongeLayerX1min, spKernel, nuLB, D3Q27System::W);
+   grid->accept(slVisitorX1min);
+
+   GbCuboid3DPtr spongeLayerX3min(new GbCuboid3D(g_minX1 + 0.2, g_minX2 - blockLength, g_minX3 - blockLength, g_maxX1 - 0.4, g_maxX2 + blockLength, g_minX3 + 0.2));
+   if (myid == 0) GbSystem3D::writeGeoObject(spongeLayerX3min.get(), pathOut + "/geo/spongeLayerX3min", WbWriterVtkXmlASCII::getInstance());
+   SpongeLayerBlockVisitor slVisitorX3min(spongeLayerX3min, kernel, nuLB, D3Q27System::B);
+   grid->accept(slVisitorX3min);
+
+   GbCuboid3DPtr spongeLayerX3max(new GbCuboid3D(g_minX1 + 0.2, g_minX2 - blockLength, g_maxX3 - 0.2, g_maxX1 - 0.4, g_maxX2 + blockLength, g_maxX3 + blockLength));
+   if (myid == 0) GbSystem3D::writeGeoObject(spongeLayerX3max.get(), pathOut + "/geo/spongeLayerX3max", WbWriterVtkXmlASCII::getInstance());
+   SpongeLayerBlockVisitor slVisitorX3max(spongeLayerX3max, kernel, nuLB, D3Q27System::T);
+   grid->accept(slVisitorX3max);*/
+   /////////////////////////////////////////////////////////////////////////////
+
+
+   vector<double>  nupsStep = { 10,10,100000 };
+   int numOfThreads = 1;
+   double outTimeStep = 10;
+   double outTimeStart = 10;
+   double endTime = 100;
+
+   SPtr<UbScheduler> nupsSch(new UbScheduler(nupsStep[0], nupsStep[1], nupsStep[2]));
+   std::shared_ptr<NUPSCounterCoProcessor> nupsCoProcessor(new NUPSCounterCoProcessor(grid, nupsSch, numOfThreads, comm));
+
+   SPtr<UbScheduler> stepSch(new UbScheduler(outTimeStep, outTimeStart));
+
+   SPtr<WriteMacroscopicQuantitiesCoProcessor> writeMQCoProcessor(new WriteMacroscopicQuantitiesCoProcessor(grid, stepSch, pathOut, WbWriterVtkXmlBinary::getInstance(), conv, comm));
+
+   SPtr<UbScheduler> stepGhostLayer(new UbScheduler(1));
+   SPtr<Calculator> calculator(new BasicCalculator(grid, stepGhostLayer, endTime));
+   calculator->addCoProcessor(nupsCoProcessor);
+   //calculator->addCoProcessor(restartCoProcessor);
+   calculator->addCoProcessor(writeMQCoProcessor);
+   /////////////////////////////////////////////////////////////////////////////////////
+
+   SPtr<IntegrateValuesHelper> mic1(new IntegrateValuesHelper(grid, comm, 0.1777, -0.004, -0.004, 0.1857, 0.004, 0.004));
+   //for check in paraview
+   if (myid == 0) GbSystem3D::writeGeoObject(mic1->getBoundingBox().get(),pathOut + "/geo/mic1", WbWriterVtkXmlBinary::getInstance());
+   SPtr<UbScheduler> stepMV(new UbScheduler(1, 0, 1000000));
+   SPtr<TimeseriesCoProcessor> tsp1(new TimeseriesCoProcessor(grid, stepMV, mic1, pathOut + "/mic/mic1", comm));
+   calculator->addCoProcessor(tsp1);
+
+   if (myid == 0) UBLOG(logINFO, "Simulation-start");
+   calculator->calculate();
+   if (myid == 0) UBLOG(logINFO, "Simulation-end");
 
 }
 
