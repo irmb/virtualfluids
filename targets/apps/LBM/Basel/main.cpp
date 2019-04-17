f3b5cb724b5b286bedc695a9e135bcf5e3fa66c0
Martin Schönherr <schoen@irmb.tu-bs.de>
2019-04-17 11:15:17 +0200

    changed the path to config file

--- targets/apps/LBM/Basel/main.cpp
+++ targets/apps/LBM/Basel/main.cpp
@@ -222,9 +222,9 @@ int main( int argc, char* argv[])
             try
             {
 				//multipleLevel("E:/temp/Basel2019/config/configBasel.txt"); //Tesla03
-				//multipleLevel("C:/Users/schoen/Desktop/bin/ReleaseBasel/configBasel.txt"); //Baumbart
+				multipleLevel("C:/Users/schoen/Desktop/bin/ReleaseBasel/configBasel.txt"); //Baumbart
 				//multipleLevel("F:/Work/Computations/gridGenerator/inp/configTest.txt");
-				multipleLevel("C:/Users/hiwi/Desktop/configBasel.txt"); //Gamling
+				//multipleLevel("C:/Users/hiwi/Desktop/configBasel.txt"); //Gamling
             }
             catch (const std::exception& e)
             {
