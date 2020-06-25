f51e62281f87b53a130384def7ece93c01b6dd09
Soeren Peters <peters@irmb.tu-bs.de>
2020-06-25 14:27:59 +0200

    Part 5.  Move original VF gpu into gpu/

--- targets/libs/GksGpu/3rdPartyLinking.cmake
+++
@@ -1,6 +0,0 @@
-include (${CMAKE_SOURCE_DIR}/${cmakeMacroPath}/Cuda/Link.cmake)
-linkCuda(${targetName})
-include (${CMAKE_SOURCE_DIR}/${cmakeMacroPath}/MPI/Link.cmake)
-linkMPI(${targetName})
-include (${CMAKE_SOURCE_DIR}/${cmakeMacroPath}/OpenMP/Link.cmake)
-linkOpenMP(${targetName})
--- targets/libs/VirtualFluids_GPU/3rdPartyLinking.cmake
+++
@@ -1,6 +0,0 @@
-include (${CMAKE_SOURCE_DIR}/${cmakeMacroPath}/Cuda/Link.cmake)
-linkCuda(${targetName})
-include (${CMAKE_SOURCE_DIR}/${cmakeMacroPath}/MPI/Link.cmake)
-linkMPI(${targetName})
-include (${CMAKE_SOURCE_DIR}/${cmakeMacroPath}/Boost/Link.cmake)
-linkBoost(${targetName} "serialization")
