f0bd275b2b54f801db57743a19d94e8452f88aed
Soeren Peters <peters@irmb.tu-bs.de>
2020-08-14 15:28:22 +0200

    - add metis path to config file

--- CMake/cmake_config_files/TESLA01.config.cmake
+++ CMake/cmake_config_files/TESLA01.config.cmake
@@ -11,3 +11,9 @@ SET(BOOST_ROOT  "C:\\Libraries\\boost_1_65_1"  CACHE PATH "BOOST_ROOT")
 SET(BOOST_LIBRARYDIR  "C:\\Libraries\\boost_1_65_1\\lib" CACHE PATH "BOOST_LIBRARYDIR")
 
 #SET(VTK_DIR "F:/Libraries/vtk/VTK-8.2.0/build" CACHE PATH "VTK directory override" FORCE)
+
+IF(${USE_METIS})
+    SET(METIS_INCLUDEDIR "C:/Libraries/metis-5.1.0//include")
+    SET(METIS_DEBUG_LIBRARY "C:/Libraries/metis-5.1.0/build/libmetis/Debug/metis.lib")
+    SET(METIS_RELEASE_LIBRARY "C:/Libraries/metis-5.1.0/build/libmetis/Release/metis.lib")
+ENDIF()
