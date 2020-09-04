bf1ded0859c3be7fb2089c30c0aaebe337dd3d83
Soeren Peters <peters@irmb.tu-bs.de>
2020-09-04 16:01:28 +0200

    Add 3rd library cmake functions.

---
+++ CMake/3rd.cmake
@@ -0,0 +1,5 @@
+include(${VF_CMAKE_DIR}/3rd/boost.cmake)
+include(${VF_CMAKE_DIR}/3rd/cuda.cmake)
+include(${VF_CMAKE_DIR}/3rd/gmock.cmake)
+include(${VF_CMAKE_DIR}/3rd/mpi.cmake)
+include(${VF_CMAKE_DIR}/3rd/OpenMP.cmake)
