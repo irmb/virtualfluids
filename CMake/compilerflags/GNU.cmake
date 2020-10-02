6b87b8a24b642ba81cd11d61d6136e56881b887a
Soeren Peters <peters@irmb.tu-bs.de>
2020-10-02 15:39:36 +0200

    Refactoring of the compiler flag files.

--- CMake/compilerflags/GNU.cmake
+++ CMake/compilerflags/GNU.cmake
@@ -1,22 +1,31 @@
+#############################################################################################################
+# compiler flags
+#############################################################################################################
 
-#############################################################################################################
-# Flags
-#############################################################################################################
-list(APPEND CAB_COMPILER_ADDTIONAL_CXX_COMPILER_FLAGS "-g")  # generates debug information. Works best with -O0.
-list(APPEND CAB_COMPILER_ADDTIONAL_CXX_COMPILER_FLAGS "-O0") # optimization level (-O3: most optimization which also could result in larger binaries)
+# debug
+list(APPEND CAB_COMPILER_ADDTIONAL_CXX_COMPILER_FLAGS_DEBUG "-g")  # generates debug information. Works best with -O0.
+list(APPEND CAB_COMPILER_ADDTIONAL_CXX_COMPILER_FLAGS_DEBUG "-O0") # no optimization
+
+# release
+list(APPEND CAB_COMPILER_ADDTIONAL_CXX_COMPILER_FLAGS_RELEASE "-O3") # optimization level (-O3: most optimization which also could result in larger binaries)
+
+# all
 list(APPEND CAB_COMPILER_ADDTIONAL_CXX_COMPILER_FLAGS "-fPIC") # position independent code for shared libraries
 LIST(APPEND CAB_COMPILER_ADDTIONAL_CXX_COMPILER_FLAGS "-funroll-all-loops")
 
+
+#############################################################################################################
+# warnings
+#############################################################################################################
 list(APPEND CAB_COMPILER_ADDTIONAL_CXX_COMPILER_FLAGS "-Wall")
 list(APPEND CAB_COMPILER_ADDTIONAL_CXX_COMPILER_FLAGS "-Wno-unused-function")
 list(APPEND CAB_COMPILER_ADDTIONAL_CXX_COMPILER_FLAGS "-Wno-reorder")
 list(APPEND CAB_COMPILER_ADDTIONAL_CXX_COMPILER_FLAGS "-Wno-sign-compare")
 
 
-#LIST(APPEND CAB_COMPILER_ADDTIONAL_CXX_COMPILER_FLAGS "-fext-numeric-literals")
-#LIST(APPEND CAB_COMPILER_ADDTIONAL_CXX_COMPILER_FLAGS "-D_GLIBCXX_USE_CXX11_ABI=0")
-
+#############################################################################################################
+# linker options
+#############################################################################################################
 list(APPEND VF_LINK_OPTIONS -lgomp)
 list(APPEND VF_LINK_OPTIONS -lrt)
-
 list(APPEND VF_LINK_OPTIONS -ldl)
--- CMake/compilerflags/MSVC.cmake
+++ CMake/compilerflags/MSVC.cmake
@@ -1,29 +1,22 @@
-###############################################################################################################
-##  MSVC
-###############################################################################################################
+#############################################################################################################
+# compiler flags
+#############################################################################################################
+list(APPEND CAB_COMPILER_ADDTIONAL_CXX_COMPILER_FLAGS "/bigobj") # increases that address capacity to 4,294,967,296 (2^32).
+list(APPEND CAB_COMPILER_ADDTIONAL_CXX_COMPILER_FLAGS "-MP")     # enable multi-threaded compiling
 
-###############################################################################################################
-## USE_UNSECURE_STL_VECTORS_RELEASE ?
-###############################################################################################################
-OPTION(USE_UNSECURE_STL_VECTORS_RELEASE "_SECURE_SCL=0" OFF)
-IF(USE_UNSECURE_STL_VECTORS_RELEASE)
-    # More MSVC specific compilation flags
-    LIST(APPEND VF_COMPILER_DEFINITION _SECURE_SCL=0)
-    LIST(APPEND VF_COMPILER_DEFINITION _SCL_SECURE_NO_WARNINGS)
-ENDIF()
 
-#LIST(APPEND CAB_COMPILER_ADDTIONAL_CXX_COMPILER_FLAGS_RELEASE "/W1")
-#LIST(APPEND CAB_COMPILER_ADDTIONAL_CXX_COMPILER_FLAGS_DEBUG "/Wall")
+#############################################################################################################
+# warnings
+#############################################################################################################
+list(APPEND CAB_COMPILER_ADDTIONAL_CXX_COMPILER_FLAGS "/wd4251") # disable needs to have dll interface
 
-###############################################################################################################
-## Flags
-###############################################################################################################
-LIST(APPEND VF_COMPILER_DEFINITION _CRT_SECURE_NO_DEPRECATE) # disable warnings promoting Microsoft's security enhanced CRT
-#LIST(APPEND VF_COMPILER_DEFINITION _SCL_SECURE_NO_WARNINGS)  # disable warnings triggered by Microsoft's checked iterators
-#LIST(APPEND CAB_COMPILER_ADDTIONAL_CXX_COMPILER_FLAGS "/wd4996") # deprecated strcpy...
-#LIST(APPEND CAB_COMPILER_ADDTIONAL_CXX_COMPILER_FLAGS "/wd4800") # forcing value to bool 'true' or 'false' (performance warning)
+#############################################################################################################
+# preprocessor definitions
+#############################################################################################################
+list(APPEND VF_COMPILER_DEFINITION _CRT_SECURE_NO_DEPRECATE) # disable warnings promoting Microsoft's security enhanced CRT
 
-LIST(APPEND CAB_COMPILER_ADDTIONAL_CXX_COMPILER_FLAGS "/bigobj") # increases that address capacity to 4,294,967,296 (2^32).
-LIST(APPEND CAB_COMPILER_ADDTIONAL_CXX_COMPILER_FLAGS "-MP") # enable multi-threaded compiling
-
-LIST(APPEND CAB_COMPILER_ADDTIONAL_CXX_COMPILER_FLAGS "/wd4251") # disable needs to have dll interface
+option(USE_UNSECURE_STL_VECTORS_RELEASE "_SECURE_SCL=0" OFF)
+if(USE_UNSECURE_STL_VECTORS_RELEASE)
+    list(APPEND VF_COMPILER_DEFINITION _SECURE_SCL=0)
+    list(APPEND VF_COMPILER_DEFINITION _SCL_SECURE_NO_WARNINGS)
+endif()
