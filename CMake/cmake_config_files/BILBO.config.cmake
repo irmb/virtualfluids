ed876cedfe5837939af90ea5c1f5d0bf22f74d2c
Soeren Peters <peters@irmb.tu-bs.de>
2020-10-06 10:33:32 +0200

    Update gitlab CI and config files.

--- CMake/cmake_config_files/BILBO.config.cmake
+++ CMake/cmake_config_files/BILBO.config.cmake
@@ -3,11 +3,3 @@
 # Responsible: Soeren Peters
 # OS:          MacOS X
 #################################################################################
-
-#################################################################################
-#  METIS
-#################################################################################
-SET(METIS_INCLUDEDIR "/usr/local/include")
-SET(METIS_DEBUG_LIBRARY "/usr/local/lib/libmetis.a")
-SET(METIS_RELEASE_LIBRARY "/usr/local/lib/libmetis.a")
-
