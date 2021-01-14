2ce440724379ec1427a9d17a88be6ed98271893d
Sven Marcus <s.marcus@outlook.de>
2021-01-14 14:55:40 +0100

    Move writeBlocks() after InteractorHelper

--- Python/poiseuille/poiseuille_hpc.py
+++ Python/poiseuille/poiseuille_hpc.py
@@ -4,13 +4,13 @@ from pyfluids.parameters import *
 grid_parameters = GridParameters()
 grid_parameters.number_of_nodes_per_direction = [64, 64, 512]
 grid_parameters.node_distance = 1
-grid_parameters.blocks_per_direction = [1, 1, 2]
+grid_parameters.blocks_per_direction = [1, 2, 2]
 
 physical_parameters = PhysicalParameters()
 physical_parameters.lattice_viscosity = 0.0005
 
 runtime_parameters = RuntimeParameters()
-runtime_parameters.number_of_threads = 1
+runtime_parameters.number_of_threads = 4
 runtime_parameters.number_of_timesteps = 1000
 runtime_parameters.timestep_log_interval = 100
 
