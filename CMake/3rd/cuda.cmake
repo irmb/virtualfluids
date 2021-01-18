3e0f996e01cacc70fff57ec5e2e1fa5099ad3ad3
Soeren Peters <peters@irmb.tu-bs.de>
2021-01-18 15:37:11 +0100

    Clone test_data for the numerical tests and artifact the output.

--- CMake/3rd/cuda.cmake
+++ CMake/3rd/cuda.cmake
@@ -11,6 +11,7 @@ function(linkCUDA)
 
     vf_get_library_name(library_name)
     target_include_directories(${library_name} PRIVATE ${CUDA_CUT_INCLUDE_DIR})
+    target_include_directories(${library_name} PRIVATE ${CMAKE_CUDA_TOOLKIT_INCLUDE_DIRECTORIES})
 
     # set the following properties only for specific targets
     # set_property(TARGET ${targetName} PROPERTY CUDA_SEPARABLE_COMPILATION ON)
