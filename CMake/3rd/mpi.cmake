cd9fc234df523174f26f3b699b13cdf45d2195df
Soeren Peters <peters.soeren@gmx.net>
2021-02-10 20:25:11 +0100

    Link MPI public by default.

--- CMake/3rd/mpi.cmake
+++ CMake/3rd/mpi.cmake
@@ -3,7 +3,6 @@ function (linkMPI)
     find_package(MPI REQUIRED)
 
     vf_get_library_name(library_name)
-    #target_include_directories(${library_name} PUBLIC ${MPI_CXX_INCLUDE_PATH})
-    target_link_libraries(${library_name} PRIVATE MPI::MPI_CXX)
+    target_link_libraries(${library_name} PUBLIC MPI::MPI_CXX)
 
 endfunction()
