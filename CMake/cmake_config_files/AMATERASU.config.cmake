4ed25b4fdaf2b8c70c9b960c1e87f5f3c02ca397
AMATERASU\geier <geier@irmb.tu-bs.de>
2021-03-10 17:00:38 +0100

    correct config file for Amaterasu to enable METIS

--- CMake/cmake_config_files/AMATERASU.config.cmake
+++ CMake/cmake_config_files/AMATERASU.config.cmake
@@ -9,6 +9,6 @@
 #################################################################################
 #  METIS  
 #################################################################################
-SET(METIS_INCLUDEDIR "C:/Users/geier/Documents/metis-5.1.0/include")
-SET(METIS_DEBUG_LIBRARY "C:/Users/geier/Documents/metis-5.1.0/build/libmetis/Debug/metis.lib")
-SET(METIS_RELEASE_LIBRARY "C:/Users/geier/Documents/metis-5.1.0/build/libmetis/Release/metis.lib")
+#SET(METIS_INCLUDEDIR "C:/Users/geier/Documents/metis-5.1.0/include")
+#SET(METIS_DEBUG_LIBRARY "C:/Users/geier/Documents/metis-5.1.0/build/libmetis/Debug/metis.lib")
+#SET(METIS_RELEASE_LIBRARY "C:/Users/geier/Documents/metis-5.1.0/build/libmetis/Release/metis.lib")
