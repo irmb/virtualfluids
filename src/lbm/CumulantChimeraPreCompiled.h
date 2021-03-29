da92c5b46be7818540075b0ff6d1a3fbc40581d7
Soeren Peters <peters@irmb.tu-bs.de>
2021-03-29 14:51:15 +0000

    Add CUDA LTO.

---
+++ src/lbm/CumulantChimeraPreCompiled.h
@@ -0,0 +1,59 @@
+#ifndef LBM_CUMULANT_CHIMERA_PRE_H
+#define LBM_CUMULANT_CHIMERA_PRE_H
+
+#ifndef __host__
+#define __host__
+#endif
+#ifndef __device__
+#define __device__
+#endif
+
+#include <cmath>
+
+#include <basics/Core/DataTypes.h>
+#include <basics/Core/RealConstants.h>
+
+#include "D3Q27.h"
+#include "Chimera.h"
+#include "MacroscopicQuantities.h"
+
+namespace VF
+{
+namespace LBM
+{
+
+struct Distribution27
+{
+    real f[27];
+
+    inline __host__ __device__ real getDensity_() const
+    {
+        return getDensity(f);
+    }
+};
+
+
+
+
+inline __host__ __device__ real abs_internal(real value)
+{
+#ifdef __CUDA_ARCH__
+    return ::abs(value);
+#else
+    return std::abs(value);
+#endif
+}
+
+
+
+//////////////////////////////////////////////////////////////////////////
+//! Cumulant K17 Kernel is based on \ref
+//! <a href="https://doi.org/10.1016/j.jcp.2017.05.040"><b>[ M. Geier et al. (2017), DOI:10.1016/j.jcp.2017.05.040 ]</b></a>
+//! and \ref
+//! <a href="https://doi.org/10.1016/j.jcp.2017.07.004"><b>[ M. Geier et al. (2017), DOI:10.1016/j.jcp.2017.07.004 ]</b></a>
+//////////////////////////////////////////////////////////////////////////
+__host__ __device__ void cumulantChimera(Distribution27& distribution, real omega, real* forces);
+
+}
+}
+#endif
