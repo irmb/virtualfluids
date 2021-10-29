284a881d8f2a98ae7648d5a436bd9acd85fdf8d5
Anna Wellmann <a.wellmann@tu-braunschweig.de>
2021-10-29 14:15:41 +0200

    Rename size of array in calculation of turbulence intensity

--- src/gpu/VirtualFluids_GPU/Calculation/CalcTurbulenceIntensity.cpp
+++ src/gpu/VirtualFluids_GPU/Calculation/CalcTurbulenceIntensity.cpp
@@ -10,21 +10,21 @@
 #include <helper_cuda.h>
 #include <basics/Core/StringUtilities/StringUtil.h>
 
-void allocTurbulenceIntensity(Parameter *para, CudaMemoryManager *cudaManager, uint size)
+void allocTurbulenceIntensity(Parameter *para, CudaMemoryManager *cudaManager, uint sizeOfTiArray)
 {
     int lev = para->getFine();
-	cudaManager->cudaAllocTurbulenceIntensity(lev, size);
-    resetVelocityFluctuationsAndMeans(para, cudaManager, size);
-    para->getParH(lev)->turbulenceIntensity.resize(size);
+	cudaManager->cudaAllocTurbulenceIntensity(lev, sizeOfTiArray);
+    resetVelocityFluctuationsAndMeans(para, cudaManager, sizeOfTiArray);
+    para->getParH(lev)->turbulenceIntensity.resize(sizeOfTiArray);
 }
 
 
-void calcVelocityAndFluctuations(Parameter *para, CudaMemoryManager *cudaManager, uint tdiff, uint size)
+void calcVelocityAndFluctuations(Parameter *para, CudaMemoryManager *cudaManager, uint tdiff, uint sizeOfTiArray)
 {
     int lev = para->getFine();
-    cudaManager->cudaCopyTurbulenceIntensityDH(lev, size);
+    cudaManager->cudaCopyTurbulenceIntensityDH(lev, sizeOfTiArray);
 
-	for (uint i = 0; i < size; i++)
+	for (uint i = 0; i < sizeOfTiArray; i++)
 	{
         // mean velocity
         para->getParH(lev)->vx_mean[i] = para->getParH(lev)->vx_mean[i]   / (real)tdiff;
@@ -43,14 +43,14 @@ void calcVelocityAndFluctuations(Parameter *para, CudaMemoryManager *cudaManager
 }
 
 
-void calcTurbulenceIntensity(Parameter *para, CudaMemoryManager *cudaManager, uint tdiff, uint size) {
+void calcTurbulenceIntensity(Parameter *para, CudaMemoryManager *cudaManager, uint tdiff, uint sizeOfTiArray) {
     
-    calcVelocityAndFluctuations(para, cudaManager, tdiff, size);
+    calcVelocityAndFluctuations(para, cudaManager, tdiff, sizeOfTiArray);
     int lev = para->getFine();
 
     real fluc_squared;
     real v_mean_squared;
-    for (uint i = 0; i < size; i++) {
+    for (uint i = 0; i < sizeOfTiArray; i++) {
         fluc_squared =
             (real)(1.0 / 3.0 * (para->getParH(lev)->vxx[i] + para->getParH(lev)->vyy[i] + para->getParH(lev)->vzz[i]));
         v_mean_squared = para->getParH(lev)->vx_mean[i] * para->getParH(lev)->vx_mean[i] +
@@ -61,11 +61,11 @@ void calcTurbulenceIntensity(Parameter *para, CudaMemoryManager *cudaManager, ui
 }
 
 
-void resetVelocityFluctuationsAndMeans(Parameter *para, CudaMemoryManager *cudaManager, uint size)
+void resetVelocityFluctuationsAndMeans(Parameter *para, CudaMemoryManager *cudaManager, uint sizeOfTiArray)
 {
     int lev = para->getFine();
 
-    for (unsigned int i = 0; i < size; i++) {
+    for (unsigned int i = 0; i < sizeOfTiArray; i++) {
         para->getParH(lev)->vxx[i]     = (real)0.0;
         para->getParH(lev)->vyy[i]     = (real)0.0;
         para->getParH(lev)->vzz[i]     = (real)0.0;
@@ -74,33 +74,33 @@ void resetVelocityFluctuationsAndMeans(Parameter *para, CudaMemoryManager *cudaM
         para->getParH(lev)->vz_mean[i] = (real)0.0;
     }
 
-    cudaManager->cudaCopyTurbulenceIntensityHD(lev, size);
+    cudaManager->cudaCopyTurbulenceIntensityHD(lev, sizeOfTiArray);
 }
 
-void writeTurbulenceIntensityToFile(Parameter *para, uint timestep, int *vectorOfSparseIndices, int size)
+void writeTurbulenceIntensityToFile(Parameter *para, uint timestep, int *vectorOfSparseIndices, int sizeOfTiArray)
 {
     int lev = para->getFine();
     std::vector<real *> data = { para->getParH(lev)->turbulenceIntensity.data() };
     std::vector<std::string> datanames = { "ti" };
-    writeTiStuffToFile(para, timestep, vectorOfSparseIndices, size, data, datanames);
+    writeTiStuffToFile(para, timestep, vectorOfSparseIndices, sizeOfTiArray, data, datanames);
 }
 
-void writeVeloFluctuationToFile(Parameter *para, uint timestep, int *vectorOfSparseIndices, int size) 
+void writeVeloFluctuationToFile(Parameter *para, uint timestep, int *vectorOfSparseIndices, int sizeOfTiArray) 
 {
     int lev                            = para->getFine();
     std::vector<real *> data           = { para->getParH(lev)->vxx, para->getParH(lev)->vyy, para->getParH(lev)->vzz };
     std::vector<std::string> datanames = { "vxx", "vyy", "vzz" };
-    writeTiStuffToFile(para, timestep, vectorOfSparseIndices, size, data, datanames);
+    writeTiStuffToFile(para, timestep, vectorOfSparseIndices, sizeOfTiArray, data, datanames);
 }
 
-void writeVeloMeansToFile(Parameter *para, uint timestep, int *vectorOfSparseIndices, int size) {
+void writeVeloMeansToFile(Parameter *para, uint timestep, int *vectorOfSparseIndices, int sizeOfTiArray) {
     int lev                            = para->getFine();
     std::vector<real *> data           = { para->getParH(lev)->vx_mean, para->getParH(lev)->vy_mean, para->getParH(lev)->vz_mean };
     std::vector<std::string> datanames = { "vx_mean", "vy_mean", "vz_mean" };
-    writeTiStuffToFile(para, timestep, vectorOfSparseIndices, size, data, datanames);
+    writeTiStuffToFile(para, timestep, vectorOfSparseIndices, sizeOfTiArray, data, datanames);
 }
 
-void writeAllTiDatafToFile(Parameter *para, uint timestep, int *vectorOfSparseIndices, int size)
+void writeAllTiDatafToFile(Parameter *para, uint timestep, int *vectorOfSparseIndices, int sizeOfTiArray)
 {
     int lev                            = para->getFine();
     std::vector<real *> data           = { para->getParH(lev)->vxx,
@@ -111,10 +111,10 @@ void writeAllTiDatafToFile(Parameter *para, uint timestep, int *vectorOfSparseIn
                                            para->getParH(lev)->vz_mean,
                                            para->getParH(lev)->turbulenceIntensity.data() };
     std::vector<std::string> datanames = { "vxx", "vyy", "vzz", "vx_mean", "vy_mean", "vz_mean", "ti" };
-    writeTiStuffToFile(para, timestep, vectorOfSparseIndices, size, data, datanames);
+    writeTiStuffToFile(para, timestep, vectorOfSparseIndices, sizeOfTiArray, data, datanames);
 }
 
-void writeTiStuffToFile(Parameter *para, uint timestep, int *vectorOfSparseIndices, int size, std::vector<real *> &data,
+void writeTiStuffToFile(Parameter *para, uint timestep, int *vectorOfSparseIndices, int sizeOfTiArray, std::vector<real *> &data,
                         std::vector<std::string> &datanames)
 {
     //////////////////////////////////////////////////////////////////////////
@@ -137,7 +137,7 @@ void writeTiStuffToFile(Parameter *para, uint timestep, int *vectorOfSparseIndic
     ostr << std::endl;
     ////////////////////////////////////////////////////////////////////////
     // fill file with data
-    for (int i = 0; i < size; i++) {
+    for (int i = 0; i < sizeOfTiArray; i++) {
         ostr << vectorOfSparseIndices[i];
         for (auto dataset : data)
             ostr << "\t" << dataset[i];
--- src/gpu/VirtualFluids_GPU/Calculation/CalcTurbulenceIntensity.h
+++ src/gpu/VirtualFluids_GPU/Calculation/CalcTurbulenceIntensity.h
@@ -6,17 +6,17 @@
 #include "Parameter/Parameter.h"
 #include "GPU/CudaMemoryManager.h"
 
-extern "C" void allocTurbulenceIntensity(Parameter *para, CudaMemoryManager *cudaManager, uint size);
-extern "C" void calcVelocityAndFluctuations(Parameter *para, CudaMemoryManager *cudaManager, uint tdiff, uint size);
-extern "C" void calcTurbulenceIntensity(Parameter *para, CudaMemoryManager *cudaManager, uint tdiff, uint size);
-extern "C" void resetVelocityFluctuationsAndMeans(Parameter *para, CudaMemoryManager *cudaManager, uint size);
+extern "C" void allocTurbulenceIntensity(Parameter *para, CudaMemoryManager *cudaManager, uint sizeOfTiArray);
+extern "C" void calcVelocityAndFluctuations(Parameter *para, CudaMemoryManager *cudaManager, uint tdiff, uint sizeOfTiArray);
+extern "C" void calcTurbulenceIntensity(Parameter *para, CudaMemoryManager *cudaManager, uint tdiff, uint sizeOfTiArray);
+extern "C" void resetVelocityFluctuationsAndMeans(Parameter *para, CudaMemoryManager *cudaManager, uint sizeOfTiArray);
 
-void writeTurbulenceIntensityToFile(Parameter *para, uint timestep, int *vectorOfSparseIndices, int size);
-void writeVeloFluctuationToFile(Parameter *para, uint timestep, int *vectorOfSparseIndices, int size);
-void writeVeloMeansToFile(Parameter *para, uint timestep, int *vectorOfSparseIndices, int size);
-void writeAllTiDatafToFile(Parameter *para, uint timestep, int *vectorOfSparseIndices, int size);
+void writeTurbulenceIntensityToFile(Parameter *para, uint timestep, int *vectorOfSparseIndices, int sizeOfTiArray);
+void writeVeloFluctuationToFile(Parameter *para, uint timestep, int *vectorOfSparseIndices, int sizeOfTiArray);
+void writeVeloMeansToFile(Parameter *para, uint timestep, int *vectorOfSparseIndices, int sizeOfTiArray);
+void writeAllTiDatafToFile(Parameter *para, uint timestep, int *vectorOfSparseIndices, int sizeOfTiArray);
 
-void writeTiStuffToFile(Parameter *para, uint timestep, int *vectorOfSparseIndices, int size, std::vector<real *> &data,
+void writeTiStuffToFile(Parameter *para, uint timestep, int *vectorOfSparseIndices, int sizeOfTiArray, std::vector<real *> &data,
                   std::vector<std::string> &datanames);
 
 #endif
