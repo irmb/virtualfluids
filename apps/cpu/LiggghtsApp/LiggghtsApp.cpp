2a214b739c36e754b35673518eb0f7410b0fce1f
kutscher <kutscher@irmb.tu-bs.de>
2021-11-18 10:28:06 +0100

    add coprocessor and wrapper for LIGGGHTS

--- apps/cpu/LiggghtsApp/LiggghtsApp.cpp
+++ apps/cpu/LiggghtsApp/LiggghtsApp.cpp
@@ -4,90 +4,96 @@
 
 #include "VirtualFluids.h"
 
-#include "lammps.h"
-#include "input.h"
-#include "atom.h"
-#include "modify.h"
-#include "fix_lb_coupling_onetoone.h"
+//#include "lammps.h"
+//#include "input.h"
+//#include "atom.h"
+//#include "modify.h"
+//#include "fix_lb_coupling_onetoone.h"
 
-#include "LiggghtsCoProcessor.h"
+#include "LiggghtsCouplingCoProcessor.h"
+
+#include "LiggghtsCouplingWrapper.h"
+
 
 using namespace std;
 
 
 int main(int argc, char *argv[])
 {
-	LAMMPS_NS::LAMMPS *lmp;
-    // custom argument vector for LAMMPS library
-    const char *lmpargv[] {"liblammps", "-log", "none"};
-    int lmpargc = sizeof(lmpargv)/sizeof(const char *);
+    SPtr<Communicator> comm = MPICommunicator::getInstance();
+    LiggghtsCouplingWrapper wrapper(argv, (MPI_Comm)comm->getNativeCommunicator());
 
-    // explicitly initialize MPI
-    MPI_Init(&argc, &argv);
+	//LAMMPS_NS::LAMMPS *lmp;
+ //   // custom argument vector for LAMMPS library
+ //   const char *lmpargv[] {"liblammps", "-log", "none"};
+ //   int lmpargc = sizeof(lmpargv)/sizeof(const char *);
 
-    // create LAMMPS instance
-    lmp = new LAMMPS_NS::LAMMPS(lmpargc, (char **)lmpargv, MPI_COMM_WORLD);
-    lmp->input->file("in.lbdem");
-    //lmp->input->one("run 1");
-    
-    //# Try extracting a global value
-    //    print("")
-    //    print("Attempting to get the number of atoms in simulation")
-    //    numAtoms = lmp.extract_global("natoms", 0)
-    //    print("natoms =", numAtoms)
+ //   // explicitly initialize MPI
+ //   MPI_Init(&argc, &argv);
 
-    //    # Try extracting atom's positions
-    //    print("")
-    //    print("Attempting to get the atom's positions")
-    //    pos = lmp.extract_atom("x",3)
-    //    for k in range(0,numAtoms):
-    //        print("Pos[%i] = [%f, %f, %f]" % (k, pos[k][0], pos[k][1], pos[k][2]))
+ //   // create LAMMPS instance
+ //   lmp = new LAMMPS_NS::LAMMPS(lmpargc, (char **)lmpargv, MPI_COMM_WORLD);
+ //   lmp->input->file("in.lbdem");
+ //   //lmp->input->one("run 1");
+ //   
+ //   //# Try extracting a global value
+ //   //    print("")
+ //   //    print("Attempting to get the number of atoms in simulation")
+ //   //    numAtoms = lmp.extract_global("natoms", 0)
+ //   //    print("natoms =", numAtoms)
 
-    LAMMPS_NS::FixLbCouplingOnetoone 
-        *couplingFix 
-        = dynamic_cast<LAMMPS_NS::FixLbCouplingOnetoone*>
-        (lmp->modify->find_fix_style("couple/lb/onetoone",0));
+ //   //    # Try extracting atom's positions
+ //   //    print("")
+ //   //    print("Attempting to get the atom's positions")
+ //   //    pos = lmp.extract_atom("x",3)
+ //   //    for k in range(0,numAtoms):
+ //   //        print("Pos[%i] = [%f, %f, %f]" % (k, pos[k][0], pos[k][1], pos[k][2]))
 
-    cout << "test1\n";
-    
-    //double **t_liggghts = couplingFix->get_torque_ptr();
-    cout << "test2\n";
+ //   LAMMPS_NS::FixLbCouplingOnetoone 
+ //       *couplingFix 
+ //       = dynamic_cast<LAMMPS_NS::FixLbCouplingOnetoone*>
+ //       (lmp->modify->find_fix_style("couple/lb/onetoone",0));
 
-    lmp->input->one("run 9 upto");
+ //   cout << "test1\n";
+ //   
+ //   //double **t_liggghts = couplingFix->get_torque_ptr();
+ //   cout << "test2\n";
 
-    for (int step = 0; step < 10; step++)
-    {
-        
+ //   lmp->input->one("run 9 upto");
 
-        int numAtoms = lmp->atom->natoms;
+ //   for (int step = 0; step < 10; step++)
+ //   {
+ //       
 
-        //double** pos = (double**)lmp->atom->extract("x");
-        double** pos = lmp->atom->x;
-        
-        //double* forceX = lmp->atom->fx;
+ //       int numAtoms = lmp->atom->natoms;
 
-        for (int i = 0; i < numAtoms; i++)
-        {
-            double **f_liggghts = couplingFix->get_force_ptr();
-            double** force = lmp->atom->f;
-            cout << "Pos[" << i << "] = [" << pos[i][0] << ", " << pos[i][1] << ", " << pos[i][2] << "]\n";
-            cout << "Force1[" << i << "] = [" << f_liggghts[i][0] << ", " << f_liggghts[i][1] << ", " << f_liggghts[i][2] << "]\n";
-            f_liggghts[i][0] += 0;
-            f_liggghts[i][1] += 0;
-            f_liggghts[i][2] += 500;
-            cout << "Force2[" << i << "] = [" << force[i][0] << ", " << force[i][1] << ", " << force[i][2] << "]\n";
-        }
+ //       //double** pos = (double**)lmp->atom->extract("x");
+ //       double** pos = lmp->atom->x;
+ //       
+ //       //double* forceX = lmp->atom->fx;
 
-        couplingFix->comm_force_torque();
+ //       for (int i = 0; i < numAtoms; i++)
+ //       {
+ //           double **f_liggghts = couplingFix->get_force_ptr();
+ //           double** force = lmp->atom->f;
+ //           cout << "Pos[" << i << "] = [" << pos[i][0] << ", " << pos[i][1] << ", " << pos[i][2] << "]\n";
+ //           cout << "Force1[" << i << "] = [" << f_liggghts[i][0] << ", " << f_liggghts[i][1] << ", " << f_liggghts[i][2] << "]\n";
+ //           f_liggghts[i][0] += 0;
+ //           f_liggghts[i][1] += 0;
+ //           f_liggghts[i][2] += 500;
+ //           cout << "Force2[" << i << "] = [" << force[i][0] << ", " << force[i][1] << ", " << force[i][2] << "]\n";
+ //       }
 
-        lmp->input->one("run 10000");
-       
-    }
+ //       couplingFix->comm_force_torque();
 
-    // delete LAMMPS instance
-    delete lmp;
+ //       lmp->input->one("run 10000");
+ //      
+ //   }
 
-    // stop MPI environment
-    MPI_Finalize();
+ //   // delete LAMMPS instance
+ //   delete lmp;
+
+ //   // stop MPI environment
+ //   MPI_Finalize();
     return 0;
 }
---
+++ src/cpu/LiggghtsCoupling/LiggghtsCouplingCoProcessor.cpp
@@ -0,0 +1,13 @@
+#include "LiggghtsCouplingCoProcessor.h"
+
+LiggghtsCouplingCoProcessor::LiggghtsCouplingCoProcessor()
+{
+}
+
+LiggghtsCouplingCoProcessor::~LiggghtsCouplingCoProcessor()
+{
+}
+
+void LiggghtsCouplingCoProcessor::process(double actualTimeStep)
+{
+}
