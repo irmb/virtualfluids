0a5c13f071d1bcb544e4547d8f560b579d69174f
Henry <henry.korb@geo.uu.se>
2022-05-17 10:35:10 +0200

    adds ABL app update ALM app

--- apps/gpu/LBM/ABL/ABL.cpp
+++ apps/gpu/LBM/ABL/ABL.cpp
@@ -52,7 +52,7 @@
 #include "VirtualFluids_GPU/PreProcessor/PreProcessorFactory/PreProcessorFactoryImp.h"
 
 #include "VirtualFluids_GPU/GPU/CudaMemoryManager.h"
-#include "VirtualFluids_GPU/PreCollisionInteractor/VelocitySetter.h"
+#include "VirtualFluids_GPU/PreCollisionInteractor/PrecursorWriter.h"
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -66,11 +66,11 @@
 
 LbmOrGks lbmOrGks = LBM;
 
-const real reference_diameter = 126.0; // diameter in m
+const real reference_height = 2000.f; // boundary layer height in m
 
-const real L_x = 10*reference_diameter;
-const real L_y = 6*reference_diameter;
-const real L_z = 6*reference_diameter;
+const real L_x = 1*reference_height;
+const real L_y = 1*reference_height;
+const real L_z = reference_height;
 
 const real viscosity = 1.56e-5;
 
@@ -78,14 +78,14 @@ const real velocity  = 9.0;
 
 const real mach = 0.1;
 
-const uint nodes_per_diameter = 16;
+const uint nodes_per_height = 5;
 
 std::string path(".");
 
-std::string simulationName("ActuatorLine");
+std::string simulationName("ABL");
 
 const float tOut = 100;
-const float tEnd = 280; // total time of simulation in s
+const float tEnd = 1000; // total time of simulation in s
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -106,12 +106,12 @@ void multipleLevel(const std::string& configPath)
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-	const real dx = reference_diameter/real(nodes_per_diameter);
+	const real dx = reference_height/real(nodes_per_height);
 
 	gridBuilder->addCoarseGrid(0.0, 0.0, 0.0,
 							   L_x,  L_y,  L_z, dx);
 
-	gridBuilder->setPeriodicBoundaryCondition(false, false, false);
+	gridBuilder->setPeriodicBoundaryCondition(true, true, false);
 
 	gridBuilder->buildGrids(lbmOrGks, false); // buildGrids() has to be called before setting the BCs!!!!
 
@@ -136,8 +136,6 @@ void multipleLevel(const std::string& configPath)
     VF_LOG_INFO("viscosity [10^8 dx^2/dt] = {}", viscosityLB*1e8);
 
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-    VelocityReader* vel_reader = new VelocityReader(path+"/Slice", "vti", 0, L_y, 0, L_z);
 
     para->setDevices(std::vector<uint>{(uint)0});
 
@@ -170,13 +168,8 @@ void multipleLevel(const std::string& configPath)
 
 
     /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    gridBuilder->setVelocityBoundaryCondition(SideType::MX,  velocityLB,  0.0, 0.0);
-
-    gridBuilder->setVelocityBoundaryCondition(SideType::MY,  velocityLB,  0.0, 0.0);
-    gridBuilder->setVelocityBoundaryCondition(SideType::PY,  velocityLB,  0.0, 0.0);
     gridBuilder->setVelocityBoundaryCondition(SideType::MZ,  velocityLB,  0.0, 0.0);
     gridBuilder->setVelocityBoundaryCondition(SideType::PZ,  velocityLB,  0.0, 0.0);
-    gridBuilder->setPressureBoundaryCondition(SideType::PX, 0.0);
 
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -184,33 +177,11 @@ void multipleLevel(const std::string& configPath)
 
     SPtr<GridProvider> gridGenerator = GridProvider::makeGridGenerator(gridBuilder, para, cudaMemoryManager);
 
-    real turbPos[3] = {3*reference_diameter, 3*reference_diameter, 3*reference_diameter};
-    real epsilon = 5.f; // width of gaussian smearing
-    real density = 1.225f;
-    int level = 0;
-    uint nBlades = 3;
-    uint nBladeNodes = 32;
-
-    SPtr<ActuatorLine> actuator_line =SPtr<ActuatorLine>( new ActuatorLine(nBlades, density, nBladeNodes, epsilon, turbPos[0], turbPos[1], turbPos[2], reference_diameter, level, dt, dx) );
-    para->addActuator( actuator_line );
-
-    SPtr<PointProbe> pointProbe = SPtr<PointProbe>( new PointProbe("pointProbe", para->getOutputPath(), 100, 500, 100) );
-    std::vector<real> probeCoordsX = {reference_diameter,2*reference_diameter,5*reference_diameter};
-    std::vector<real> probeCoordsY = {3*reference_diameter,3*reference_diameter,3*reference_diameter};
-    std::vector<real> probeCoordsZ = {3*reference_diameter,3*reference_diameter,3*reference_diameter};
-    pointProbe->addProbePointsFromList(probeCoordsX, probeCoordsY, probeCoordsZ);
-    // pointProbe->addProbePointsFromXNormalPlane(2*D, 0.0, 0.0, L_y, L_z, (uint)L_y/dx, (uint)L_z/dx);
-    pointProbe->addPostProcessingVariable(PostProcessingVariable::Means);
-    pointProbe->addPostProcessingVariable(PostProcessingVariable::Variances);
-    para->addProbe( pointProbe );
-
-    SPtr<PlaneProbe> planeProbe = SPtr<PlaneProbe>( new PlaneProbe("planeProbe", para->getOutputPath(), 100, 500, 100) );
-    planeProbe->setProbePlane(5*reference_diameter, 0, 0, dx, L_y, L_z);
-    planeProbe->addPostProcessingVariable(PostProcessingVariable::Means);
-    para->addProbe( planeProbe );
+    real x_pos = L_x/2;
 
 
-
+    auto precursor_writer = SPtr<PrecursorWriter>( new PrecursorWriter("Precursor", "output", x_pos, 0, L_y, 0, L_z, 10, 5, 40) );
+    para->addProbe( precursor_writer );
 
     Simulation sim(communicator);
     SPtr<FileWriter> fileWriter = SPtr<FileWriter>(new FileWriter());
@@ -232,7 +203,7 @@ int main( int argc, char* argv[])
 
             if( argc > 1){ path = argv[1]; }
 
-            multipleLevel(path + "/configActuatorLine.txt");
+            multipleLevel(path + "/configABL.txt");
         }
         catch (const spdlog::spdlog_ex &ex) {
             std::cout << "Log initialization failed: " << ex.what() << std::endl;
