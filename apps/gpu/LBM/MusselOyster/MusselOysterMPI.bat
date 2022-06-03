f3034094ca814d288fbd8fde895f308282a692d4
Anna Wellmann <a.wellmann@tu-bs.de>
2022-06-03 06:56:17 +0000

    Remove MusselOyster.bat

--- apps/gpu/LBM/MusselOyster/MusselOysterMPI.bat
+++
@@ -1,4 +0,0 @@
-:: don't close cmd on error
-if not defined in_subprocess (cmd /k set in_subprocess=y ^& %0 %*) & exit )
-:: @ECHO OFF
-mpiexec -n 2 C:\Users\Master\Documents\MasterAnna\VirtualFluids_dev\build\bin\Release\MusselOyster.exe
