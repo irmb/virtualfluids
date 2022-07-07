c20311afc3f84201946ef92bcc8327d6505f5b64
Anna Wellmann <a.wellmann@tu-braunschweig.de>
2022-07-07 17:28:31 +0200

    Rename distributions D3Q27 in GridGenerator

--- src/gpu/GridGenerator/grid/distributions/Distribution.cpp
+++ src/gpu/GridGenerator/grid/distributions/Distribution.cpp
@@ -42,154 +42,154 @@ Distribution DistributionHelper::getDistribution27()
 {
     Distribution d27;
     d27.name = "D3Q27";
-    d27.dir_start = DIR_27_START;
-    d27.dir_end = DIR_27_END;
+    d27.dir_start = STARTDIR;
+    d27.dir_end = ENDDIR;
 
-    d27.dirs = new int[(DIR_27_END + 1) * DIMENSION];
+    d27.dirs = new int[(ENDDIR + 1) * DIMENSION];
 
-    d27.directions = new Direction[DIR_27_END + 1];
-    d27.directions[DIR_27_E] = Direction(DIR_27_E_X, DIR_27_E_Y, DIR_27_E_Z);
-    d27.directions[DIR_27_W] = Direction(DIR_27_W_X, DIR_27_W_Y, DIR_27_W_Z);
-    d27.directions[DIR_27_N] = Direction(DIR_27_N_X, DIR_27_N_Y, DIR_27_N_Z);
-    d27.directions[DIR_27_S] = Direction(DIR_27_S_X, DIR_27_S_Y, DIR_27_S_Z);
-    d27.directions[DIR_27_T] = Direction(DIR_27_T_X, DIR_27_T_Y, DIR_27_T_Z);
-    d27.directions[DIR_27_B] = Direction(DIR_27_B_X, DIR_27_B_Y, DIR_27_B_Z);
+    d27.directions = new Direction[ENDDIR + 1];
+    d27.directions[E] = Direction(DIR_27_E_X, DIR_27_E_Y, DIR_27_E_Z);
+    d27.directions[W] = Direction(DIR_27_W_X, DIR_27_W_Y, DIR_27_W_Z);
+    d27.directions[N] = Direction(DIR_27_N_X, DIR_27_N_Y, DIR_27_N_Z);
+    d27.directions[S] = Direction(DIR_27_S_X, DIR_27_S_Y, DIR_27_S_Z);
+    d27.directions[T] = Direction(DIR_27_T_X, DIR_27_T_Y, DIR_27_T_Z);
+    d27.directions[B] = Direction(DIR_27_B_X, DIR_27_B_Y, DIR_27_B_Z);
 
-    d27.directions[DIR_27_NE] = Direction(DIR_27_NE_X, DIR_27_NE_Y, DIR_27_NE_Z);
-    d27.directions[DIR_27_SW] = Direction(DIR_27_SW_X, DIR_27_SW_Y, DIR_27_SW_Z);
-    d27.directions[DIR_27_SE] = Direction(DIR_27_SE_X, DIR_27_SE_Y, DIR_27_SE_Z);
-    d27.directions[DIR_27_NW] = Direction(DIR_27_NW_X, DIR_27_NW_Y, DIR_27_NW_Z);
+    d27.directions[NE] = Direction(DIR_27_NE_X, DIR_27_NE_Y, DIR_27_NE_Z);
+    d27.directions[SW] = Direction(DIR_27_SW_X, DIR_27_SW_Y, DIR_27_SW_Z);
+    d27.directions[SE] = Direction(DIR_27_SE_X, DIR_27_SE_Y, DIR_27_SE_Z);
+    d27.directions[NW] = Direction(DIR_27_NW_X, DIR_27_NW_Y, DIR_27_NW_Z);
 
-    d27.directions[DIR_27_TE] = Direction(DIR_27_TE_X, DIR_27_TE_Y, DIR_27_TE_Z);
-    d27.directions[DIR_27_BW] = Direction(DIR_27_BW_X, DIR_27_BW_Y, DIR_27_BW_Z);
-    d27.directions[DIR_27_BE] = Direction(DIR_27_BE_X, DIR_27_BE_Y, DIR_27_BE_Z);
-    d27.directions[DIR_27_TW] = Direction(DIR_27_TW_X, DIR_27_TW_Y, DIR_27_TW_Z);
+    d27.directions[TE] = Direction(DIR_27_TE_X, DIR_27_TE_Y, DIR_27_TE_Z);
+    d27.directions[BW] = Direction(DIR_27_BW_X, DIR_27_BW_Y, DIR_27_BW_Z);
+    d27.directions[BE] = Direction(DIR_27_BE_X, DIR_27_BE_Y, DIR_27_BE_Z);
+    d27.directions[TW] = Direction(DIR_27_TW_X, DIR_27_TW_Y, DIR_27_TW_Z);
 
-    d27.directions[DIR_27_TN] = Direction(DIR_27_TN_X, DIR_27_TN_Y, DIR_27_TN_Z);
-    d27.directions[DIR_27_BS] = Direction(DIR_27_BS_X, DIR_27_BS_Y, DIR_27_BS_Z);
-    d27.directions[DIR_27_BN] = Direction(DIR_27_BN_X, DIR_27_BN_Y, DIR_27_BN_Z);
-    d27.directions[DIR_27_TS] = Direction(DIR_27_TS_X, DIR_27_TS_Y, DIR_27_TS_Z);
+    d27.directions[TN] = Direction(DIR_27_TN_X, DIR_27_TN_Y, DIR_27_TN_Z);
+    d27.directions[BS] = Direction(DIR_27_BS_X, DIR_27_BS_Y, DIR_27_BS_Z);
+    d27.directions[BN] = Direction(DIR_27_BN_X, DIR_27_BN_Y, DIR_27_BN_Z);
+    d27.directions[TS] = Direction(DIR_27_TS_X, DIR_27_TS_Y, DIR_27_TS_Z);
 
-    d27.directions[DIR_27_REST] = Direction(DIR_27_REST_X, DIR_27_REST_Y, DIR_27_REST_Z);
+    d27.directions[REST] = Direction(DIR_27_REST_X, DIR_27_REST_Y, DIR_27_REST_Z);
 
-    d27.directions[DIR_27_TNE] = Direction(DIR_27_TNE_X, DIR_27_TNE_Y, DIR_27_TNE_Z);
-    d27.directions[DIR_27_TNW] = Direction(DIR_27_TNW_X, DIR_27_TNW_Y, DIR_27_TNW_Z);
-    d27.directions[DIR_27_TSE] = Direction(DIR_27_TSE_X, DIR_27_TSE_Y, DIR_27_TSE_Z);
-    d27.directions[DIR_27_TSW] = Direction(DIR_27_TSW_X, DIR_27_TSW_Y, DIR_27_TSW_Z);
+    d27.directions[TNE] = Direction(DIR_27_TNE_X, DIR_27_TNE_Y, DIR_27_TNE_Z);
+    d27.directions[TNW] = Direction(DIR_27_TNW_X, DIR_27_TNW_Y, DIR_27_TNW_Z);
+    d27.directions[TSE] = Direction(DIR_27_TSE_X, DIR_27_TSE_Y, DIR_27_TSE_Z);
+    d27.directions[TSW] = Direction(DIR_27_TSW_X, DIR_27_TSW_Y, DIR_27_TSW_Z);
 
-    d27.directions[DIR_27_BNE] = Direction(DIR_27_BNE_X, DIR_27_BNE_Y, DIR_27_BNE_Z);
-    d27.directions[DIR_27_BNW] = Direction(DIR_27_BNW_X, DIR_27_BNW_Y, DIR_27_BNW_Z);
-    d27.directions[DIR_27_BSE] = Direction(DIR_27_BSE_X, DIR_27_BSE_Y, DIR_27_BSE_Z);
-    d27.directions[DIR_27_BSW] = Direction(DIR_27_BSW_X, DIR_27_BSW_Y, DIR_27_BSW_Z);
+    d27.directions[BNE] = Direction(DIR_27_BNE_X, DIR_27_BNE_Y, DIR_27_BNE_Z);
+    d27.directions[BNW] = Direction(DIR_27_BNW_X, DIR_27_BNW_Y, DIR_27_BNW_Z);
+    d27.directions[BSE] = Direction(DIR_27_BSE_X, DIR_27_BSE_Y, DIR_27_BSE_Z);
+    d27.directions[BSW] = Direction(DIR_27_BSW_X, DIR_27_BSW_Y, DIR_27_BSW_Z);
 
 
-    d27.dirs[DIR_27_E * 3    ] = DIR_27_E_X;
-    d27.dirs[DIR_27_E * 3 + 1] = DIR_27_E_Y;
-    d27.dirs[DIR_27_E * 3 + 2] = DIR_27_E_Z;
+    d27.dirs[E * 3    ] = DIR_27_E_X;
+    d27.dirs[E * 3 + 1] = DIR_27_E_Y;
+    d27.dirs[E * 3 + 2] = DIR_27_E_Z;
 
-    d27.dirs[DIR_27_W * 3    ] = DIR_27_W_X;
-    d27.dirs[DIR_27_W * 3 + 1] = DIR_27_W_Y;
-    d27.dirs[DIR_27_W * 3 + 2] = DIR_27_W_Z;
+    d27.dirs[W * 3    ] = DIR_27_W_X;
+    d27.dirs[W * 3 + 1] = DIR_27_W_Y;
+    d27.dirs[W * 3 + 2] = DIR_27_W_Z;
     
-    d27.dirs[DIR_27_N * 3    ] = DIR_27_N_X;
-    d27.dirs[DIR_27_N * 3 + 1] = DIR_27_N_Y;
-    d27.dirs[DIR_27_N * 3 + 2] = DIR_27_N_Z;
+    d27.dirs[N * 3    ] = DIR_27_N_X;
+    d27.dirs[N * 3 + 1] = DIR_27_N_Y;
+    d27.dirs[N * 3 + 2] = DIR_27_N_Z;
 
-    d27.dirs[DIR_27_S * 3    ] = DIR_27_S_X;
-    d27.dirs[DIR_27_S * 3 + 1] = DIR_27_S_Y;
-    d27.dirs[DIR_27_S * 3 + 2] = DIR_27_S_Z;
+    d27.dirs[S * 3    ] = DIR_27_S_X;
+    d27.dirs[S * 3 + 1] = DIR_27_S_Y;
+    d27.dirs[S * 3 + 2] = DIR_27_S_Z;
     
-    d27.dirs[DIR_27_T * 3    ] = DIR_27_T_X;
-    d27.dirs[DIR_27_T * 3 + 1] = DIR_27_T_Y;
-    d27.dirs[DIR_27_T * 3 + 2] = DIR_27_T_Z;
+    d27.dirs[T * 3    ] = DIR_27_T_X;
+    d27.dirs[T * 3 + 1] = DIR_27_T_Y;
+    d27.dirs[T * 3 + 2] = DIR_27_T_Z;
     
-    d27.dirs[DIR_27_B * 3    ] = DIR_27_B_X;
-    d27.dirs[DIR_27_B * 3 + 1] = DIR_27_B_Y;
-    d27.dirs[DIR_27_B * 3 + 2] = DIR_27_B_Z;
+    d27.dirs[B * 3    ] = DIR_27_B_X;
+    d27.dirs[B * 3 + 1] = DIR_27_B_Y;
+    d27.dirs[B * 3 + 2] = DIR_27_B_Z;
 
-    d27.dirs[DIR_27_NE * 3    ] = DIR_27_NE_X;
-    d27.dirs[DIR_27_NE * 3 + 1] = DIR_27_NE_Y;
-    d27.dirs[DIR_27_NE * 3 + 2] = DIR_27_NE_Z;
+    d27.dirs[NE * 3    ] = DIR_27_NE_X;
+    d27.dirs[NE * 3 + 1] = DIR_27_NE_Y;
+    d27.dirs[NE * 3 + 2] = DIR_27_NE_Z;
     
-    d27.dirs[DIR_27_SW * 3    ] = DIR_27_SW_X;
-    d27.dirs[DIR_27_SW * 3 + 1] = DIR_27_SW_Y;
-    d27.dirs[DIR_27_SW * 3 + 2] = DIR_27_SW_Z;
+    d27.dirs[SW * 3    ] = DIR_27_SW_X;
+    d27.dirs[SW * 3 + 1] = DIR_27_SW_Y;
+    d27.dirs[SW * 3 + 2] = DIR_27_SW_Z;
 
-    d27.dirs[DIR_27_SE * 3    ] = DIR_27_SE_X;
-    d27.dirs[DIR_27_SE * 3 + 1] = DIR_27_SE_Y;
-    d27.dirs[DIR_27_SE * 3 + 2] = DIR_27_SE_Z;
+    d27.dirs[SE * 3    ] = DIR_27_SE_X;
+    d27.dirs[SE * 3 + 1] = DIR_27_SE_Y;
+    d27.dirs[SE * 3 + 2] = DIR_27_SE_Z;
 
-    d27.dirs[DIR_27_NW * 3    ] = DIR_27_NW_X;
-    d27.dirs[DIR_27_NW * 3 + 1] = DIR_27_NW_Y;
-    d27.dirs[DIR_27_NW * 3 + 2] = DIR_27_NW_Z;
+    d27.dirs[NW * 3    ] = DIR_27_NW_X;
+    d27.dirs[NW * 3 + 1] = DIR_27_NW_Y;
+    d27.dirs[NW * 3 + 2] = DIR_27_NW_Z;
 
-    d27.dirs[DIR_27_TE * 3    ] = DIR_27_TE_X;
-    d27.dirs[DIR_27_TE * 3 + 1] = DIR_27_TE_Y;
-    d27.dirs[DIR_27_TE * 3 + 2] = DIR_27_TE_Z;
+    d27.dirs[TE * 3    ] = DIR_27_TE_X;
+    d27.dirs[TE * 3 + 1] = DIR_27_TE_Y;
+    d27.dirs[TE * 3 + 2] = DIR_27_TE_Z;
 
-    d27.dirs[DIR_27_BW * 3    ] = DIR_27_BW_X;
-    d27.dirs[DIR_27_BW * 3 + 1] = DIR_27_BW_Y;
-    d27.dirs[DIR_27_BW * 3 + 2] = DIR_27_BW_Z;
+    d27.dirs[BW * 3    ] = DIR_27_BW_X;
+    d27.dirs[BW * 3 + 1] = DIR_27_BW_Y;
+    d27.dirs[BW * 3 + 2] = DIR_27_BW_Z;
                               
-    d27.dirs[DIR_27_BE * 3    ] = DIR_27_BE_X;
-    d27.dirs[DIR_27_BE * 3 + 1] = DIR_27_BE_Y;
-    d27.dirs[DIR_27_BE * 3 + 2] = DIR_27_BE_Z;
+    d27.dirs[BE * 3    ] = DIR_27_BE_X;
+    d27.dirs[BE * 3 + 1] = DIR_27_BE_Y;
+    d27.dirs[BE * 3 + 2] = DIR_27_BE_Z;
                               
-    d27.dirs[DIR_27_TW * 3    ] = DIR_27_TW_X;
-    d27.dirs[DIR_27_TW * 3 + 1] = DIR_27_TW_Y;
-    d27.dirs[DIR_27_TW * 3 + 2] = DIR_27_TW_Z;
+    d27.dirs[TW * 3    ] = DIR_27_TW_X;
+    d27.dirs[TW * 3 + 1] = DIR_27_TW_Y;
+    d27.dirs[TW * 3 + 2] = DIR_27_TW_Z;
                               
-    d27.dirs[DIR_27_TN * 3    ] = DIR_27_TN_X;
-    d27.dirs[DIR_27_TN * 3 + 1] = DIR_27_TN_Y;
-    d27.dirs[DIR_27_TN * 3 + 2] = DIR_27_TN_Z;
+    d27.dirs[TN * 3    ] = DIR_27_TN_X;
+    d27.dirs[TN * 3 + 1] = DIR_27_TN_Y;
+    d27.dirs[TN * 3 + 2] = DIR_27_TN_Z;
                               
-    d27.dirs[DIR_27_BS * 3    ] = DIR_27_BS_X;
-    d27.dirs[DIR_27_BS * 3 + 1] = DIR_27_BS_Y;
-    d27.dirs[DIR_27_BS * 3 + 2] = DIR_27_BS_Z;
+    d27.dirs[BS * 3    ] = DIR_27_BS_X;
+    d27.dirs[BS * 3 + 1] = DIR_27_BS_Y;
+    d27.dirs[BS * 3 + 2] = DIR_27_BS_Z;
                               
-    d27.dirs[DIR_27_BN * 3    ] = DIR_27_BN_X;
-    d27.dirs[DIR_27_BN * 3 + 1] = DIR_27_BN_Y;
-    d27.dirs[DIR_27_BN * 3 + 2] = DIR_27_BN_Z;
+    d27.dirs[BN * 3    ] = DIR_27_BN_X;
+    d27.dirs[BN * 3 + 1] = DIR_27_BN_Y;
+    d27.dirs[BN * 3 + 2] = DIR_27_BN_Z;
 
-    d27.dirs[DIR_27_TS * 3    ] = DIR_27_TS_X;
-    d27.dirs[DIR_27_TS * 3 + 1] = DIR_27_TS_Y;
-    d27.dirs[DIR_27_TS * 3 + 2] = DIR_27_TS_Z;
+    d27.dirs[TS * 3    ] = DIR_27_TS_X;
+    d27.dirs[TS * 3 + 1] = DIR_27_TS_Y;
+    d27.dirs[TS * 3 + 2] = DIR_27_TS_Z;
 
-    d27.dirs[DIR_27_REST * 3    ] = DIR_27_REST_X;   //
-    d27.dirs[DIR_27_REST * 3 + 1] = DIR_27_REST_Y;   //  ZERO ELEMENT
-    d27.dirs[DIR_27_REST * 3 + 2] = DIR_27_REST_Z;   //
+    d27.dirs[REST * 3    ] = DIR_27_REST_X;   //
+    d27.dirs[REST * 3 + 1] = DIR_27_REST_Y;   //  ZERO ELEMENT
+    d27.dirs[REST * 3 + 2] = DIR_27_REST_Z;   //
 
-    d27.dirs[DIR_27_TNE * 3    ] = DIR_27_TNE_X;
-    d27.dirs[DIR_27_TNE * 3 + 1] = DIR_27_TNE_Y;
-    d27.dirs[DIR_27_TNE * 3 + 2] = DIR_27_TNE_Z;
+    d27.dirs[TNE * 3    ] = DIR_27_TNE_X;
+    d27.dirs[TNE * 3 + 1] = DIR_27_TNE_Y;
+    d27.dirs[TNE * 3 + 2] = DIR_27_TNE_Z;
 
-    d27.dirs[DIR_27_BNE * 3    ] = DIR_27_BNE_X;
-    d27.dirs[DIR_27_BNE * 3 + 1] = DIR_27_BNE_Y;
-    d27.dirs[DIR_27_BNE * 3 + 2] = DIR_27_BNE_Z;
+    d27.dirs[BNE * 3    ] = DIR_27_BNE_X;
+    d27.dirs[BNE * 3 + 1] = DIR_27_BNE_Y;
+    d27.dirs[BNE * 3 + 2] = DIR_27_BNE_Z;
 
-    d27.dirs[DIR_27_TSE * 3    ] = DIR_27_TSE_X;
-    d27.dirs[DIR_27_TSE * 3 + 1] = DIR_27_TSE_Y;
-    d27.dirs[DIR_27_TSE * 3 + 2] = DIR_27_TSE_Z;
+    d27.dirs[TSE * 3    ] = DIR_27_TSE_X;
+    d27.dirs[TSE * 3 + 1] = DIR_27_TSE_Y;
+    d27.dirs[TSE * 3 + 2] = DIR_27_TSE_Z;
 
-    d27.dirs[DIR_27_BSE * 3    ] = DIR_27_BSE_X;
-    d27.dirs[DIR_27_BSE * 3 + 1] = DIR_27_BSE_Y;
-    d27.dirs[DIR_27_BSE * 3 + 2] = DIR_27_BSE_Z;
+    d27.dirs[BSE * 3    ] = DIR_27_BSE_X;
+    d27.dirs[BSE * 3 + 1] = DIR_27_BSE_Y;
+    d27.dirs[BSE * 3 + 2] = DIR_27_BSE_Z;
 
-    d27.dirs[DIR_27_TNW * 3    ] = DIR_27_TNW_X;
-    d27.dirs[DIR_27_TNW * 3 + 1] = DIR_27_TNW_Y;
-    d27.dirs[DIR_27_TNW * 3 + 2] = DIR_27_TNW_Z;
+    d27.dirs[TNW * 3    ] = DIR_27_TNW_X;
+    d27.dirs[TNW * 3 + 1] = DIR_27_TNW_Y;
+    d27.dirs[TNW * 3 + 2] = DIR_27_TNW_Z;
 
-    d27.dirs[DIR_27_BNW * 3    ] = DIR_27_BNW_X;
-    d27.dirs[DIR_27_BNW * 3 + 1] = DIR_27_BNW_Y;
-    d27.dirs[DIR_27_BNW * 3 + 2] = DIR_27_BNW_Z;
+    d27.dirs[BNW * 3    ] = DIR_27_BNW_X;
+    d27.dirs[BNW * 3 + 1] = DIR_27_BNW_Y;
+    d27.dirs[BNW * 3 + 2] = DIR_27_BNW_Z;
 
-    d27.dirs[DIR_27_TSW * 3    ] = DIR_27_TSW_X;
-    d27.dirs[DIR_27_TSW * 3 + 1] = DIR_27_TSW_Y;
-    d27.dirs[DIR_27_TSW * 3 + 2] = DIR_27_TSW_Z;
+    d27.dirs[TSW * 3    ] = DIR_27_TSW_X;
+    d27.dirs[TSW * 3 + 1] = DIR_27_TSW_Y;
+    d27.dirs[TSW * 3 + 2] = DIR_27_TSW_Z;
 
-    d27.dirs[DIR_27_BSW * 3    ] = DIR_27_BSW_X;
-    d27.dirs[DIR_27_BSW * 3 + 1] = DIR_27_BSW_Y;
-    d27.dirs[DIR_27_BSW * 3 + 2] = DIR_27_BSW_Z;
+    d27.dirs[BSW * 3    ] = DIR_27_BSW_X;
+    d27.dirs[BSW * 3 + 1] = DIR_27_BSW_Y;
+    d27.dirs[BSW * 3 + 2] = DIR_27_BSW_Z;
 
     return d27;
 }
--- src/gpu/GridGenerator/grid/distributions/DistributionTest.cpp
+++ src/gpu/GridGenerator/grid/distributions/DistributionTest.cpp
@@ -8,52 +8,52 @@ TEST(DistributionTest, DistributionReturnsCorrectDirections)
 {
     Distribution dist = DistributionHelper::getDistribution27();
 
-    EXPECT_THAT(dist.directions[DIR_27_E][0], testing::Eq(DIR_27_E_X));
-    EXPECT_THAT(dist.directions[DIR_27_E][1], testing::Eq(DIR_27_E_Y));
-    EXPECT_THAT(dist.directions[DIR_27_E][2], testing::Eq(DIR_27_E_Z));
-    EXPECT_THAT(dist.dirs[DIR_27_E * 3    ], testing::Eq(DIR_27_E_X));
-    EXPECT_THAT(dist.dirs[DIR_27_E * 3 + 1], testing::Eq(DIR_27_E_Y));
-    EXPECT_THAT(dist.dirs[DIR_27_E * 3 + 2], testing::Eq(DIR_27_E_Z));
+    EXPECT_THAT(dist.directions[E][0], testing::Eq(DIR_27_E_X));
+    EXPECT_THAT(dist.directions[E][1], testing::Eq(DIR_27_E_Y));
+    EXPECT_THAT(dist.directions[E][2], testing::Eq(DIR_27_E_Z));
+    EXPECT_THAT(dist.dirs[E * 3    ], testing::Eq(DIR_27_E_X));
+    EXPECT_THAT(dist.dirs[E * 3 + 1], testing::Eq(DIR_27_E_Y));
+    EXPECT_THAT(dist.dirs[E * 3 + 2], testing::Eq(DIR_27_E_Z));
 
-    EXPECT_THAT(dist.directions[DIR_27_B][0], testing::Eq(DIR_27_B_X));
-    EXPECT_THAT(dist.directions[DIR_27_B][1], testing::Eq(DIR_27_B_Y));
-    EXPECT_THAT(dist.directions[DIR_27_B][2], testing::Eq(DIR_27_B_Z));
-    EXPECT_THAT(dist.dirs[DIR_27_B * 3    ], testing::Eq(DIR_27_B_X));
-    EXPECT_THAT(dist.dirs[DIR_27_B * 3 + 1], testing::Eq(DIR_27_B_Y));
-    EXPECT_THAT(dist.dirs[DIR_27_B * 3 + 2], testing::Eq(DIR_27_B_Z));
+    EXPECT_THAT(dist.directions[B][0], testing::Eq(DIR_27_B_X));
+    EXPECT_THAT(dist.directions[B][1], testing::Eq(DIR_27_B_Y));
+    EXPECT_THAT(dist.directions[B][2], testing::Eq(DIR_27_B_Z));
+    EXPECT_THAT(dist.dirs[B * 3    ], testing::Eq(DIR_27_B_X));
+    EXPECT_THAT(dist.dirs[B * 3 + 1], testing::Eq(DIR_27_B_Y));
+    EXPECT_THAT(dist.dirs[B * 3 + 2], testing::Eq(DIR_27_B_Z));
     
-    EXPECT_THAT(dist.directions[DIR_27_REST][0], testing::Eq(0));
-    EXPECT_THAT(dist.directions[DIR_27_REST][1], testing::Eq(0));
-    EXPECT_THAT(dist.directions[DIR_27_REST][2], testing::Eq(0));
-    EXPECT_THAT(dist.dirs[DIR_27_REST * 3    ], testing::Eq(0));
-    EXPECT_THAT(dist.dirs[DIR_27_REST * 3 + 1], testing::Eq(0));
-    EXPECT_THAT(dist.dirs[DIR_27_REST * 3 + 2], testing::Eq(0));
+    EXPECT_THAT(dist.directions[REST][0], testing::Eq(0));
+    EXPECT_THAT(dist.directions[REST][1], testing::Eq(0));
+    EXPECT_THAT(dist.directions[REST][2], testing::Eq(0));
+    EXPECT_THAT(dist.dirs[REST * 3    ], testing::Eq(0));
+    EXPECT_THAT(dist.dirs[REST * 3 + 1], testing::Eq(0));
+    EXPECT_THAT(dist.dirs[REST * 3 + 2], testing::Eq(0));
 
-    EXPECT_THAT(dist.directions[DIR_27_NE][0], testing::Eq(DIR_27_NE_X));
-    EXPECT_THAT(dist.directions[DIR_27_NE][1], testing::Eq(DIR_27_NE_Y));
-    EXPECT_THAT(dist.directions[DIR_27_NE][2], testing::Eq(DIR_27_NE_Z));
-    EXPECT_THAT(dist.dirs[DIR_27_NE * 3    ], testing::Eq(DIR_27_NE_X));
-    EXPECT_THAT(dist.dirs[DIR_27_NE * 3 + 1], testing::Eq(DIR_27_NE_Y));
-    EXPECT_THAT(dist.dirs[DIR_27_NE * 3 + 2], testing::Eq(DIR_27_NE_Z));
+    EXPECT_THAT(dist.directions[NE][0], testing::Eq(DIR_27_NE_X));
+    EXPECT_THAT(dist.directions[NE][1], testing::Eq(DIR_27_NE_Y));
+    EXPECT_THAT(dist.directions[NE][2], testing::Eq(DIR_27_NE_Z));
+    EXPECT_THAT(dist.dirs[NE * 3    ], testing::Eq(DIR_27_NE_X));
+    EXPECT_THAT(dist.dirs[NE * 3 + 1], testing::Eq(DIR_27_NE_Y));
+    EXPECT_THAT(dist.dirs[NE * 3 + 2], testing::Eq(DIR_27_NE_Z));
 
-    EXPECT_THAT(dist.directions[DIR_27_TS][0], testing::Eq(DIR_27_TS_X));
-    EXPECT_THAT(dist.directions[DIR_27_TS][1], testing::Eq(DIR_27_TS_Y));
-    EXPECT_THAT(dist.directions[DIR_27_TS][2], testing::Eq(DIR_27_TS_Z));
-    EXPECT_THAT(dist.dirs[DIR_27_TS * 3    ], testing::Eq(DIR_27_TS_X));
-    EXPECT_THAT(dist.dirs[DIR_27_TS * 3 + 1], testing::Eq(DIR_27_TS_Y));
-    EXPECT_THAT(dist.dirs[DIR_27_TS * 3 + 2], testing::Eq(DIR_27_TS_Z));
+    EXPECT_THAT(dist.directions[TS][0], testing::Eq(DIR_27_TS_X));
+    EXPECT_THAT(dist.directions[TS][1], testing::Eq(DIR_27_TS_Y));
+    EXPECT_THAT(dist.directions[TS][2], testing::Eq(DIR_27_TS_Z));
+    EXPECT_THAT(dist.dirs[TS * 3    ], testing::Eq(DIR_27_TS_X));
+    EXPECT_THAT(dist.dirs[TS * 3 + 1], testing::Eq(DIR_27_TS_Y));
+    EXPECT_THAT(dist.dirs[TS * 3 + 2], testing::Eq(DIR_27_TS_Z));
 
-    EXPECT_THAT(dist.directions[DIR_27_TNE][0], testing::Eq(DIR_27_TNE_X));
-    EXPECT_THAT(dist.directions[DIR_27_TNE][1], testing::Eq(DIR_27_TNE_Y));
-    EXPECT_THAT(dist.directions[DIR_27_TNE][2], testing::Eq(DIR_27_TNE_Z));
-    EXPECT_THAT(dist.dirs[DIR_27_TNE * 3    ], testing::Eq(DIR_27_TNE_X));
-    EXPECT_THAT(dist.dirs[DIR_27_TNE * 3 + 1], testing::Eq(DIR_27_TNE_Y));
-    EXPECT_THAT(dist.dirs[DIR_27_TNE * 3 + 2], testing::Eq(DIR_27_TNE_Z));
+    EXPECT_THAT(dist.directions[TNE][0], testing::Eq(DIR_27_TNE_X));
+    EXPECT_THAT(dist.directions[TNE][1], testing::Eq(DIR_27_TNE_Y));
+    EXPECT_THAT(dist.directions[TNE][2], testing::Eq(DIR_27_TNE_Z));
+    EXPECT_THAT(dist.dirs[TNE * 3    ], testing::Eq(DIR_27_TNE_X));
+    EXPECT_THAT(dist.dirs[TNE * 3 + 1], testing::Eq(DIR_27_TNE_Y));
+    EXPECT_THAT(dist.dirs[TNE * 3 + 2], testing::Eq(DIR_27_TNE_Z));
 
-    EXPECT_THAT(dist.directions[DIR_27_BSW][0], testing::Eq(DIR_27_BSW_X));
-    EXPECT_THAT(dist.directions[DIR_27_BSW][1], testing::Eq(DIR_27_BSW_Y));
-    EXPECT_THAT(dist.directions[DIR_27_BSW][2], testing::Eq(DIR_27_BSW_Z));
-    EXPECT_THAT(dist.dirs[DIR_27_BSW * 3    ], testing::Eq(DIR_27_BSW_X));
-    EXPECT_THAT(dist.dirs[DIR_27_BSW * 3 + 1], testing::Eq(DIR_27_BSW_Y));
-    EXPECT_THAT(dist.dirs[DIR_27_BSW * 3 + 2], testing::Eq(DIR_27_BSW_Z));
+    EXPECT_THAT(dist.directions[BSW][0], testing::Eq(DIR_27_BSW_X));
+    EXPECT_THAT(dist.directions[BSW][1], testing::Eq(DIR_27_BSW_Y));
+    EXPECT_THAT(dist.directions[BSW][2], testing::Eq(DIR_27_BSW_Z));
+    EXPECT_THAT(dist.dirs[BSW * 3    ], testing::Eq(DIR_27_BSW_X));
+    EXPECT_THAT(dist.dirs[BSW * 3 + 1], testing::Eq(DIR_27_BSW_Y));
+    EXPECT_THAT(dist.dirs[BSW * 3 + 2], testing::Eq(DIR_27_BSW_Z));
 }
