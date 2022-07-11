cbf7a6c7988b35baa95ce111b619d58807fdf894
Anna Wellmann <a.wellmann@tu-bs.de>
2022-07-11 07:17:10 +0000

    Remove duplicate code in CudaGrid

--- src/cuda/CudaGrid.cpp
+++ src/cuda/CudaGrid.cpp
@@ -7,17 +7,7 @@ namespace vf::cuda
 
 CudaGrid::CudaGrid(unsigned int numberOfThreads, unsigned int numberOfEntities)
 {
-    unsigned int Grid = (numberOfEntities / numberOfThreads) + 1;
-    unsigned int Grid1, Grid2;
-    if (Grid > 512) {
-        Grid1 = 512;
-        Grid2 = (Grid / Grid1) + 1;
-    } else {
-        Grid1 = 1;
-        Grid2 = Grid;
-    }
-    
-    grid = dim3(Grid1, Grid2);
+    grid = getCudaGrid( numberOfThreads, numberOfEntities);
     threads = dim3(numberOfThreads, 1, 1);
 }
 
