eea1f78847e5b25b4c70ed49fee0acfc6ee57095
Soeren Peters <peters@irmb.tu-bs.de>
2022-07-21 08:02:46 +0000

    clean up machine files

--- CMake/cmake_config_files/MOLLOK.config.cmake
+++ CMake/cmake_config_files/MOLLOK.config.cmake
@@ -4,11 +4,12 @@
 # OS:          Windows 10
 #################################################################################
 
-#SET TO CORRECT PATH:
+# cuda compute capability
 set(CMAKE_CUDA_ARCHITECTURES 86)
 
+# numerical tests location of the grids
 set(PATH_NUMERICAL_TESTS "D:/out/numericalTests/")
 list(APPEND VF_COMPILER_DEFINITION "PATH_NUMERICAL_TESTS=${PATH_NUMERICAL_TESTS}")
 
-
+# add invidual apps here
 list(APPEND USER_APPS "apps/gpu/LBM/WTG_RUB")
