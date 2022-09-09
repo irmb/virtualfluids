9e0d37e6ca640b443ee774755adc3f59137513ad
HenrikAsmuth <henrik.asmuth@geo.uu.se>
2022-09-09 15:53:58 +0200

    another test

--- src/gpu/GridGenerator/grid/GridBuilder/LevelGridBuilder.h
+++ src/gpu/GridGenerator/grid/GridBuilder/LevelGridBuilder.h
@@ -45,8 +45,6 @@
 #include "gpu/GridGenerator/grid/GridInterface.h"
 #include "gpu/GridGenerator/grid/NodeValues.h"
 
-#include "../VirtualFluids_GPU/PreCollisionInteractor/VelocitySetter.h"
-
 struct Vertex;
 class  Grid;
 class Transformator;
