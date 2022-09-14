b3971e4487d832199ee6ec035872a7d179a5c708
Anna Wellmann <a.wellmann@tu-bs.de>
2022-09-14 13:52:27 +0000

    Remove fine grid from Driven Cavity

--- apps/gpu/LBM/DrivenCavity/DrivenCavity.cpp
+++ apps/gpu/LBM/DrivenCavity/DrivenCavity.cpp
@@ -115,8 +115,6 @@ int main()
 
         gridBuilder->addCoarseGrid(-0.5 * L, -0.5 * L, -0.5 * L, 0.5 * L, 0.5 * L, 0.5 * L, dx);
 
-        gridBuilder->addGrid(new Cuboid(-0.25, -0.25, -0.25, 0.25, 0.25, 0.25), 1); // add fine grid
-
         gridBuilder->setPeriodicBoundaryCondition(false, false, false);
 
         gridBuilder->buildGrids(LbmOrGks::LBM, false);
