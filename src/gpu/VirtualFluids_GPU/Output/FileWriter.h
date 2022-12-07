b2ce7edba60a9f3a69d9524c29e628ac94311a08
Henry <henry.korb@geo.uu.se>
2022-12-07 16:55:12 +0100

    simplify filewriter and add optional quantities

--- src/gpu/VirtualFluids_GPU/Output/FileWriter.h
+++ src/gpu/VirtualFluids_GPU/Output/FileWriter.h
@@ -21,17 +21,17 @@ public:
 
 private:
 	void writeTimestep(std::shared_ptr<Parameter> para, unsigned int timestep, int level) override;
-	//void writeParticle(Parameter* para, unsigned int t);
-    void writeUnstrucuredGridLT(std::shared_ptr<Parameter> para, int level,
+    std::vector<std::string> writeUnstrucuredGridLT(std::shared_ptr<Parameter> para, int level,
                                                          std::vector<std::string> &fname);
-	void writeUnstrucuredGridLTConc(std::shared_ptr<Parameter> para, int level, std::vector<std::string >& fname);
-	void writeUnstrucuredGridMedianLT(std::shared_ptr<Parameter> para, int level, std::vector<std::string >& fname);
-	void writeUnstrucuredGridMedianLTConc(std::shared_ptr<Parameter> para, int level, std::vector<std::string >& fname);
+	std::vector<std::string> writeUnstrucuredGridMedianLT(std::shared_ptr<Parameter> para, int level, std::vector<std::string >& fname);
 	bool isPeriodicCell(std::shared_ptr<Parameter> para, int level, unsigned int number2, unsigned int number1, unsigned int number3, unsigned int number5);
 
-    void writeCollectionFile( std::shared_ptr<Parameter> para, unsigned int timestep );
+    std::string writeCollectionFile( std::shared_ptr<Parameter> para, unsigned int timestep );
 
-    void writeCollectionFileMedian( std::shared_ptr<Parameter> para, unsigned int timestep );
+    std::string writeCollectionFileMedian( std::shared_ptr<Parameter> para, unsigned int timestep );
+
+	std::vector<std::string> getNodeDataNames(std::shared_ptr<Parameter> para);
+	std::vector<std::string> getMedianNodeDataNames(std::shared_ptr<Parameter> para);
 
     std::vector< std::string > fileNamesForCollectionFile;
     std::vector< std::string > fileNamesForCollectionFileMedian;
