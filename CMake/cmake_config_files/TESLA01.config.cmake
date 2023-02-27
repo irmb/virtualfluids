234748155e1eb953452ad9ac05576db3f9ca9b69
Hussein Alihussein <hussein@irmb.tu-bs.de>
2023-02-27 19:09:24 +0100

    Add cmake config files

---
+++ CMake/cmake_config_files/TESLA01.config.cmake
@@ -0,0 +1,11 @@
+#################################################################################
+# VirtualFluids MACHINE FILE
+# Responsible: Soeren Peters
+# OS:          Windows 10
+#################################################################################
+
+SET(BOOST_ROOT  "C:\\Libraries\\boost_1_65_1"  CACHE PATH "BOOST_ROOT")
+SET(BOOST_LIBRARYDIR  "C:\\Libraries\\boost_1_65_1\\lib" CACHE PATH "BOOST_LIBRARYDIR")
+
+SET(VTK_DIR "C:/Libraries/VTK-8.0.1/build")
+
