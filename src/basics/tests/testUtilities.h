b4220f4c9f5d92ed036d30182ed924426dcd3b49
Anna Wellmann <a.wellmann@tu-bs.de>
2023-04-11 12:52:48 +0000

    Add missing ;

--- src/basics/tests/testUtilities.h
+++ src/basics/tests/testUtilities.h
@@ -15,7 +15,7 @@ inline auto RealEq = [](auto value) {
 
 inline auto RealNear = [](auto value, auto max_abs_error) {
 #ifdef VF_DOUBLE_ACCURACY
-    return testing::DoubleNear(value, max_abs_error)
+    return testing::DoubleNear(value, max_abs_error);
 #else
     return testing::FloatNear(value, max_abs_error);
 #endif
