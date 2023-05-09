865238d34c828c7b64c4f6d6341b9bcd5f2e0861
Henry <henry.korb@geo.uu.se>
2023-05-09 15:36:37 +0200

    reverts !f99a64f4

--- CMake/cmake_config_files/TWIST-CFD-0009.config.cmake
+++
@@ -1,5 +0,0 @@
-SET(CMAKE_CUDA_ARCHITECTURES "86")
-SET(CMAKE_CUDA_COMPILER /usr/local/cuda/bin/nvcc)
-
-list(APPEND USER_APPS "apps/gpu/LBM/ActuatorLine")
-list(APPEND USER_APPS "apps/gpu/LBM/SphereScaling")
