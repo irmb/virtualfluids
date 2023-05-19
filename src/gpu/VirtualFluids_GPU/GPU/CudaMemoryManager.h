0e35762a8739dffa422dde71392f6b950c10e09f
Soeren Peters <peters@irmb.tu-bs.de>
2023-05-19 10:55:33 +0200

    Make clang defs only in clang env.

--- src/gpu/VirtualFluids_GPU/GPU/CudaMemoryManager.h
+++ src/gpu/VirtualFluids_GPU/GPU/CudaMemoryManager.h
@@ -14,11 +14,15 @@
 #include <helper_cuda.h>
 
 #include <curand.h>
+#ifdef __clang__
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wgnu-zero-variadic-macro-arguments"
 #pragma clang diagnostic ignored "-Wunused-but-set-parameter"
+#endif
 #include <curand_kernel.h>
+#ifdef __clang__
 #pragma clang diagnostic pop
+#endif
 
 class Parameter;
 class PorousMedia;
