c465376e0b746e15b6267ce015cc2d6e87d2d4f8
Henry <henry.korb@geo.uu.se>
2023-06-15 18:44:42 +0200

    refactor Python bindings

--- pythonbindings/src/lbm/lbm.cpp
+++ pythonbindings/src/lbm/lbm.cpp
@@ -32,14 +32,11 @@
 //=======================================================================================
 #include <pybind11/pybind11.h>
 
-namespace lbm
+namespace lbm_bindings
 {
     namespace py = pybind11;
 
-    py::module makeModule(py::module_ &parentModule)
+    PYBIND11_MODULE(lbm, m)
     {
-        py::module lbmModule = parentModule.def_submodule("lbm");
-
-        return lbmModule;
     }
 }
--- pythonbindings/src/logger.cpp
+++ pythonbindings/src/logger.cpp
@@ -33,25 +33,23 @@
 #include <pybind11/pybind11.h>
 #include <logger/Logger.h>
 
-namespace logging
+namespace logger_bindings
 {
+
     namespace py = pybind11;
 
-    py::module makeModule(py::module_ &parentModule)
+    PYBIND11_MODULE(logger, m)
     {
-        py::module loggerModule = parentModule.def_submodule("logger");
-
-        py::class_<vf::logging::Logger>(loggerModule, "Logger")
+        py::class_<vf::logging::Logger>(m, "Logger")
         .def_static("initialize_logger", &vf::logging::Logger::initializeLogger)
         .def_static("change_log_path", &vf::logging::Logger::changeLogPath, py::arg("path"));
 
         // use f-strings (f"text {float}") in python for compounded messages
-        loggerModule.def("vf_log_trace", [](std::string message){ VF_LOG_TRACE(message); }, py::arg("message"));        
-        loggerModule.def("vf_log_debug", [](std::string message){ VF_LOG_DEBUG(message); }, py::arg("message"));        
-        loggerModule.def("vf_log_info", [](std::string message){ VF_LOG_INFO(message); }, py::arg("message"));        
-        loggerModule.def("vf_log_warning", [](std::string message){ VF_LOG_WARNING(message); }, py::arg("message"));        
-        loggerModule.def("vf_log_critical", [](std::string message){ VF_LOG_CRITICAL(message); }, py::arg("message"));        
-
-        return loggerModule;
+        m.def("vf_log_trace", [](std::string message){ VF_LOG_TRACE(message); }, py::arg("message"));        
+        m.def("vf_log_debug", [](std::string message){ VF_LOG_DEBUG(message); }, py::arg("message"));        
+        m.def("vf_log_info", [](std::string message){ VF_LOG_INFO(message); }, py::arg("message"));        
+        m.def("vf_log_warning", [](std::string message){ VF_LOG_WARNING(message); }, py::arg("message"));        
+        m.def("vf_log_critical", [](std::string message){ VF_LOG_CRITICAL(message); }, py::arg("message"));       
     }
-} // namespace logging
+}
+
