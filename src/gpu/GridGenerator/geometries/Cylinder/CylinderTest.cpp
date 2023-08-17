285777270d742a68634fb6ea2aeb789aa0b8ffe8
Anna Wellmann <a.wellmann@tu-braunschweig.de>
2023-08-17 06:38:46 +0000

    Use variables in cylinder tests for object dimensions

--- src/gpu/GridGenerator/geometries/Cylinder/CylinderTest.cpp
+++ src/gpu/GridGenerator/geometries/Cylinder/CylinderTest.cpp
@@ -7,28 +7,31 @@
 class CylinderTestAxisNormalToX : public testing::Test
 {
 protected:
-    Cylinder cylinder = Cylinder({ 0.1, 0.2, 0.3 }, 2.0, 8.0, Cylinder::PrincipalAxis::x);
+    std::array<double, 3> center = { 0.1, 0.2, 0.3 };
+    double radius = 2.0;
+    double height = 8.0;
+    Cylinder cylinder = Cylinder(center[0], center[1], center[2], radius, height, Cylinder::PrincipalAxis::x);
 };
 
 TEST_F(CylinderTestAxisNormalToX, getCentroid)
 {
-    EXPECT_THAT(cylinder.getX1Centroid(), testing::Eq(0.1));
-    EXPECT_THAT(cylinder.getX2Centroid(), testing::Eq(0.2));
-    EXPECT_THAT(cylinder.getX3Centroid(), testing::Eq(0.3));
+    EXPECT_THAT(cylinder.getX1Centroid(), testing::Eq(center[0]));
+    EXPECT_THAT(cylinder.getX2Centroid(), testing::Eq(center[1]));
+    EXPECT_THAT(cylinder.getX3Centroid(), testing::Eq(center[2]));
 }
 
 TEST_F(CylinderTestAxisNormalToX, getMinimum)
 {
-    EXPECT_THAT(cylinder.getX1Minimum(), testing::Eq(-4.0 + 0.1));
-    EXPECT_THAT(cylinder.getX2Minimum(), testing::Eq(-2.0 + 0.2));
-    EXPECT_THAT(cylinder.getX3Minimum(), testing::Eq(-2.0 + 0.3));
+    EXPECT_THAT(cylinder.getX1Minimum(), testing::Eq(-0.5 * height + center[0]));
+    EXPECT_THAT(cylinder.getX2Minimum(), testing::Eq(-radius + center[1]));
+    EXPECT_THAT(cylinder.getX3Minimum(), testing::Eq(-radius + center[2]));
 }
 
 TEST_F(CylinderTestAxisNormalToX, getMaximum)
 {
-    EXPECT_THAT(cylinder.getX1Maximum(), testing::Eq(4.0 + 0.1));
-    EXPECT_THAT(cylinder.getX2Maximum(), testing::Eq(2.0 + 0.2));
-    EXPECT_THAT(cylinder.getX3Maximum(), testing::Eq(2.0 + 0.3));
+    EXPECT_THAT(cylinder.getX1Maximum(), testing::Eq(0.5 * height + center[0]));
+    EXPECT_THAT(cylinder.getX2Maximum(), testing::Eq(radius + center[1]));
+    EXPECT_THAT(cylinder.getX3Maximum(), testing::Eq(radius + center[2]));
 }
 
 // CylinderTestAxisNormalToY ////////////////////////////////////////////////
@@ -36,28 +39,31 @@ TEST_F(CylinderTestAxisNormalToX, getMaximum)
 class CylinderTestAxisNormalToY : public testing::Test
 {
 protected:
-    Cylinder cylinder = Cylinder({ 0.1, 0.2, 0.3 }, 2.0, 8.0, Cylinder::PrincipalAxis::y);
+    std::array<double, 3> center = { 0.1, 0.2, 0.3 };
+    double radius = 2.0;
+    double height = 8.0;
+    Cylinder cylinder = Cylinder({ center[0], center[1], center[2] }, radius, height, Cylinder::PrincipalAxis::y);
 };
 
 TEST_F(CylinderTestAxisNormalToY, getCentroid)
 {
-    EXPECT_THAT(cylinder.getX1Centroid(), testing::Eq(0.1));
-    EXPECT_THAT(cylinder.getX2Centroid(), testing::Eq(0.2));
-    EXPECT_THAT(cylinder.getX3Centroid(), testing::Eq(0.3));
+    EXPECT_THAT(cylinder.getX1Centroid(), testing::Eq(center[0]));
+    EXPECT_THAT(cylinder.getX2Centroid(), testing::Eq(center[1]));
+    EXPECT_THAT(cylinder.getX3Centroid(), testing::Eq(center[2]));
 }
 
 TEST_F(CylinderTestAxisNormalToY, getMinimum)
 {
-    EXPECT_THAT(cylinder.getX1Minimum(), testing::Eq(-2.0 + 0.1));
-    EXPECT_THAT(cylinder.getX2Minimum(), testing::Eq(-4.0 + 0.2));
-    EXPECT_THAT(cylinder.getX3Minimum(), testing::Eq(-2.0 + 0.3));
+    EXPECT_THAT(cylinder.getX1Minimum(), testing::Eq(-radius + center[0]));
+    EXPECT_THAT(cylinder.getX2Minimum(), testing::Eq(-0.5 * height + center[1]));
+    EXPECT_THAT(cylinder.getX3Minimum(), testing::Eq(-radius + center[2]));
 }
 
 TEST_F(CylinderTestAxisNormalToY, getMaximum)
 {
-    EXPECT_THAT(cylinder.getX1Maximum(), testing::Eq(2.0 + 0.1));
-    EXPECT_THAT(cylinder.getX2Maximum(), testing::Eq(4.0 + 0.2));
-    EXPECT_THAT(cylinder.getX3Maximum(), testing::Eq(2.0 + 0.3));
+    EXPECT_THAT(cylinder.getX1Maximum(), testing::Eq(radius + center[0]));
+    EXPECT_THAT(cylinder.getX2Maximum(), testing::Eq(0.5 * height + center[1]));
+    EXPECT_THAT(cylinder.getX3Maximum(), testing::Eq(radius + center[2]));
 }
 
 // CylinderTestAxisNormalToZ ////////////////////////////////////////////////
@@ -65,26 +71,29 @@ TEST_F(CylinderTestAxisNormalToY, getMaximum)
 class CylinderTestAxisNormalToZ : public testing::Test
 {
 protected:
-    Cylinder cylinder = Cylinder({ 0.1, 0.2, 0.3 }, 2.0, 8.0, Cylinder::PrincipalAxis::z);
+    std::array<double, 3> center = { 0.1, 0.2, 0.3 };
+    double radius = 2.0;
+    double height = 8.0;
+    Cylinder cylinder = Cylinder({ center[0], center[1], center[2] }, radius, height, Cylinder::PrincipalAxis::z);
 };
 
 TEST_F(CylinderTestAxisNormalToZ, getCentroid)
 {
-    EXPECT_THAT(cylinder.getX1Centroid(), testing::Eq(0.1));
-    EXPECT_THAT(cylinder.getX2Centroid(), testing::Eq(0.2));
-    EXPECT_THAT(cylinder.getX3Centroid(), testing::Eq(0.3));
+    EXPECT_THAT(cylinder.getX1Centroid(), testing::Eq(center[0]));
+    EXPECT_THAT(cylinder.getX2Centroid(), testing::Eq(center[1]));
+    EXPECT_THAT(cylinder.getX3Centroid(), testing::Eq(center[2]));
 }
 
 TEST_F(CylinderTestAxisNormalToZ, getMinimum)
 {
-    EXPECT_THAT(cylinder.getX1Minimum(), testing::Eq(-2.0 + 0.1));
-    EXPECT_THAT(cylinder.getX2Minimum(), testing::Eq(-2.0 + 0.2));
-    EXPECT_THAT(cylinder.getX3Minimum(), testing::Eq(-4.0 + 0.3));
+    EXPECT_THAT(cylinder.getX1Minimum(), testing::Eq(-radius + center[0]));
+    EXPECT_THAT(cylinder.getX2Minimum(), testing::Eq(-radius + center[1]));
+    EXPECT_THAT(cylinder.getX3Minimum(), testing::Eq(-0.5 * height + center[2]));
 }
 
 TEST_F(CylinderTestAxisNormalToZ, getMaximum)
 {
-    EXPECT_THAT(cylinder.getX1Maximum(), testing::Eq(2.0 + 0.1));
-    EXPECT_THAT(cylinder.getX2Maximum(), testing::Eq(2.0 + 0.2));
-    EXPECT_THAT(cylinder.getX3Maximum(), testing::Eq(4.0 + 0.3));
+    EXPECT_THAT(cylinder.getX1Maximum(), testing::Eq(radius + center[0]));
+    EXPECT_THAT(cylinder.getX2Maximum(), testing::Eq(radius + center[1]));
+    EXPECT_THAT(cylinder.getX3Maximum(), testing::Eq(0.5 * height + center[2]));
 }
--- src/gpu/GridGenerator/geometries/VerticalCylinder/VerticalCylinderTest.cpp
+++ src/gpu/GridGenerator/geometries/VerticalCylinder/VerticalCylinderTest.cpp
@@ -5,26 +5,31 @@
 class VerticalCylinderTest : public testing::Test
 {
 protected:
-    VerticalCylinder cylinder = VerticalCylinder(0.1, 0.2, 0.3, 2.0, 8.0);
+    std::array<double, 3> center = { 0.1, 0.2, 0.3 };
+    double radius = 2.0;
+    double height = 8.0;
+    VerticalCylinder cylinder = VerticalCylinder(center[0], center[1], center[2], radius, height);
 };
 
 TEST_F(VerticalCylinderTest, getCentroid)
 {
-    EXPECT_THAT(cylinder.getX1Centroid(), testing::Eq(0.1));
-    EXPECT_THAT(cylinder.getX2Centroid(), testing::Eq(0.2));
-    EXPECT_THAT(cylinder.getX3Centroid(), testing::Eq(0.3));
+    EXPECT_THAT(cylinder.getX1Centroid(), testing::Eq(center[0]));
+    EXPECT_THAT(cylinder.getX2Centroid(), testing::Eq(center[1]));
+    EXPECT_THAT(cylinder.getX3Centroid(), testing::Eq(center[2]));
 }
 
 TEST_F(VerticalCylinderTest, getMinimum)
 {
-    EXPECT_THAT(cylinder.getX1Minimum(), testing::Eq(-2.0 + 0.1));
-    EXPECT_THAT(cylinder.getX2Minimum(), testing::Eq(-2.0 + 0.2));
-    EXPECT_THAT(cylinder.getX3Minimum(), testing::Eq(-4.0 + 0.3));
+    EXPECT_THAT(cylinder.getX1Minimum(), testing::Eq(-radius + center[0]));
+    EXPECT_THAT(cylinder.getX2Minimum(), testing::Eq(-radius + center[1]));
+    EXPECT_THAT(cylinder.getX3Minimum(), testing::Eq(-0.5 * height + center[2]));
 }
 
 TEST_F(VerticalCylinderTest, getMaximum)
 {
-    EXPECT_THAT(cylinder.getX1Maximum(), testing::Eq(2.0 + 0.1));
-    EXPECT_THAT(cylinder.getX2Maximum(), testing::Eq(2.0 + 0.2));
-    EXPECT_THAT(cylinder.getX3Maximum(), testing::Eq(4.0 + 0.3));
+    EXPECT_THAT(cylinder.getX1Maximum(), testing::Eq(radius + center[0]));
+    EXPECT_THAT(cylinder.getX2Maximum(), testing::Eq(radius + center[1]));
+    EXPECT_THAT(cylinder.getX3Maximum(), testing::Eq(0.5 * height + center[2]));
 }
+
+// TEST_F()
