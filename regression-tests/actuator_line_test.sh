c8c9f6ce5f6e05d6341c38d34eb9a1e1af00f89e
Anna Wellmann <a.wellmann@tu-braunschweig.de>
2023-09-12 09:38:24 +0000

    Add regression test for actuator line

---
+++ regression-tests/actuator_line_test.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+source ./regression-tests/__regression_test_executer.sh
+
+
+# 1. set reference data directory (must match the folder structure in https://github.com/irmb/test_data)
+REFERENCE_DATA_DIR=regression_tests/gpu/ActuatorLine
+
+# 2. set cmake flags for the build of VirtualFluids
+CMAKE_FLAGS="--preset=make_gpu -DCMAKE_BUILD_TYPE=Release -DCMAKE_CUDA_ARCHITECTURES=75 -DUSER_APPS=apps/gpu/ActuatorLineRegression"
+
+# 3. define the application to be executed
+APPLICATION=./build/bin/ActuatorLineRegression
+
+# 4. set the path to the produced data
+RESULT_DATA_DIR=output/ActuatorLine
+
+
+run_regression_test "$REFERENCE_DATA_DIR" "$CMAKE_FLAGS" "$APPLICATION" "$RESULT_DATA_DIR"
+
