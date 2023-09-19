e5e0d209a8f8e578e03018973f1203d3602e98ed
Soeren Peters <peters@irmb.tu-bs.de>
2023-09-19 10:02:21 +0000

    Update path to regression test for cpu test.

--- regression-tests/flow_around_cylinder_test.sh
+++ regression-tests/flow_around_cylinder_test.sh
@@ -4,7 +4,7 @@ source ./regression-tests/__regression_test_executer.sh
 
 
 # 1. set reference data directory (must match the folder structure in https://github.com/irmb/test_data)
-REFERENCE_DATA_DIR=regression_tests/cpu/FlowAroundCylinder_2023_06
+REFERENCE_DATA_DIR=regression_tests/cpu/FlowAroundCylinder_2023_09
 
 # 2. set cmake flags for the build of VirtualFluids
 CMAKE_FLAGS="--preset=make_cpu -DCMAKE_BUILD_TYPE=Release"
