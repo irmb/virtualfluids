d281023332119fadf3446ebbe65cbd666f53f072
Soeren Peters <peters@irmb.tu-bs.de>
2023-11-07 15:10:59 +0000

    ...

--- src/lbm/constants/D3Q27.h
+++ src/lbm/constants/D3Q27.h
@@ -1,17 +1,13 @@
 #ifndef LBM_D3Q27_H
 #define LBM_D3Q27_H
 
-#include <map>
-
 #include <basics/DataTypes.h>
 
 namespace vf::lbm::dir
 {
-
 static constexpr size_t STARTDIR = 0;
 static constexpr size_t ENDDIR = 26;
 
-// used in the CPU and the GPU version
 static constexpr size_t d000 = 0;
 static constexpr size_t dP00 = 1;
 static constexpr size_t dM00 = 2;
@@ -66,47 +62,6 @@ static constexpr size_t iPPM = dMMP;
 static constexpr size_t iMPM = dPMP;
 static constexpr size_t iPMM = dMPP;
 static constexpr size_t iMMM = dPPP;
-
-struct countersForPointerChasing
-{
-    uint counterInverse;
-    uint counterX;
-    uint counterY;
-    uint counterZ;
-};
-
-const std::map<const size_t, const countersForPointerChasing> mapForPointerChasing = 
-{
-    {d000, countersForPointerChasing{0, 0, 0, 0}},
-    {dP00, countersForPointerChasing{0, 1, 0, 0}},
-    {dM00, countersForPointerChasing{1, 0, 1, 1}},
-    {d0P0, countersForPointerChasing{0, 0, 1, 0}},
-    {d0M0, countersForPointerChasing{1, 1, 0, 1}},
-    {d00P, countersForPointerChasing{0, 0, 0, 1}},
-    {d00M, countersForPointerChasing{1, 1, 1, 0}},
-
-    {dPP0, countersForPointerChasing{0, 1, 1, 0}},
-    {dMM0, countersForPointerChasing{1, 0, 0, 1}},
-    {dPM0, countersForPointerChasing{1, 2, 0, 1}},
-    {dMP0, countersForPointerChasing{1, 0, 2, 1}},
-    {dP0P, countersForPointerChasing{0, 1, 0, 1}},
-    {dM0M, countersForPointerChasing{1, 0, 1, 0}},
-    {dP0M, countersForPointerChasing{1, 2, 1, 0}},
-    {dM0P, countersForPointerChasing{1, 0, 1, 2}},
-    {d0PP, countersForPointerChasing{0, 0, 1, 1}},
-    {d0MM, countersForPointerChasing{1, 1, 0, 0}},
-    {d0PM, countersForPointerChasing{1, 1, 2, 0}},
-    {d0MP, countersForPointerChasing{1, 1, 0, 2}},
-
-    {dPPP, countersForPointerChasing{0, 1, 1, 1}},
-    {dMPP, countersForPointerChasing{1, 0, 2, 2}},
-    {dPMP, countersForPointerChasing{1, 2, 0, 2}},
-    {dMMP, countersForPointerChasing{1, 0, 0, 2}},
-    {dPPM, countersForPointerChasing{1, 2, 2, 0}},
-    {dMPM, countersForPointerChasing{1, 0, 2, 0}},
-    {dPMM, countersForPointerChasing{1, 2, 0, 0}},
-    {dMMM, countersForPointerChasing{1, 0, 0, 0}}
-};
 
 }
 #endif
