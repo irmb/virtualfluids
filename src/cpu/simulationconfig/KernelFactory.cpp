429f57a33c9cce328e48636237c89c24dc7d179b
Soeren Peters <peters@irmb.tu-bs.de>
2023-11-16 10:15:55 +0000

    Fix bindings once more.

--- src/cpu/simulationconfig/KernelFactory.cpp
+++ src/cpu/simulationconfig/KernelFactory.cpp
@@ -1,6 +1,6 @@
 #include <LBM/LBMKernel.h>
 #include <LBM/K17CompressibleNavierStokes.h>
-#include <LBM/BGKLBMKernel.h>
+#include <LBM/B92IncompressibleNavierStokes.h>
 #include <simulationconfig/D3Q27LBMSystem.h>
 #include "simulationconfig/KernelFactory.h"
 
@@ -8,7 +8,7 @@ std::shared_ptr<LBMKernel> KernelFactory::makeKernel(KernelType kernelType)
 {
     switch (kernelType) {
         case BGK:
-            return std::shared_ptr<LBMKernel>(new BGKLBMKernel());
+            return std::shared_ptr<LBMKernel>(new B92IncompressibleNavierStokes());
         case COMPRESSIBLE_CUMULANT_4TH_ORDER_VISCOSITY:
             return std::shared_ptr<LBMKernel>(new K17CompressibleNavierStokes());
         default:
