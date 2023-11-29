c1be214280a269dbf42955085d22e244c6e9a4ed
Hussein Alihussein <h.alihussein@tu-braunschweig.de>
2023-11-29 08:22:57 +0100

    Add old gyroid implicit representation

--- src/cpu/VirtualFluids.h
+++ src/cpu/VirtualFluids.h
@@ -225,6 +225,7 @@
 #include <geometry3d/GbHalfSpace3D.h>
 #include <geometry3d/GbHalfSpaceKrischan3D.h>
 #include <geometry3d/GbGyroidThirdOrder.h>
+#include <geometry3d/GbGyroidThirdOrderLong.h>
 #include <geometry3d/GbGeneralThirdOrderImplicitSurface.h>
 #include <geometry3d/GbLine3D.h>
 #include <geometry3d/GbMeshTools3D.h>
