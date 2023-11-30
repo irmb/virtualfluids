e6a377a4ae6d936d381cb9919651fa558761979e
Hussein Alihussein <h.alihussein@tu-braunschweig.de>
2023-11-30 13:29:39 +0100

    GyroidsRow regression tests

---
+++ regression-tests/gyroidsrow_test.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+source ./regression-tests/__regression_test_executer.sh
+
+
+# 1. set reference data directory (must match the folder structure in https://github.com/irmb/test_data)
+REFERENCE_DATA_DIR=regression_tests/cpu/GyroidsRow
+
+# 2. set cmake flags for the build of VirtualFluids
+CMAKE_FLAGS="--preset=make_cpu -DBUILD_USE_BOOST=ON -DCMAKE_BUILD_TYPE=Release"
+
+# 3. define the application to be executed
+APPLICATION="./build/bin/GyroidsRow ./apps/cpu/GyroidsRow/GyroidsRow_Re_425_u_001_N_100_compressible_regression.cfg"
+
+# 4. set the path to the produced data
+RESULT_DATA_DIR=output/GyroidsRow
+
+apt install libboost-all-dev -y
+
+run_regression_test "$REFERENCE_DATA_DIR" "$CMAKE_FLAGS" "$APPLICATION" "$RESULT_DATA_DIR"
