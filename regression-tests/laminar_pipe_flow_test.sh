0d890e00d448d55981463080a4e355bbe5605bee
Kutscher <kutscher@irmb.tu-bs.de>
2023-12-04 16:50:06 +0100

    update regression test for Laminar Pipe Flow

--- regression-tests/laminar_pipe_flow_test.sh
+++ regression-tests/laminar_pipe_flow_test.sh
@@ -9,7 +9,7 @@ REFERENCE_DATA_DIR=regression_tests/cpu/LaminarPipeFlow
 CMAKE_FLAGS="--preset=make_cpu -DCMAKE_BUILD_TYPE=Release"
 
 # 3. define the application to be executed
-APPLICATION="./build/bin/LaminarPipeFlow ./apps/cpu/LaminarPipeFlow/LaminarPipeFlow.cfg"
+APPLICATION="mpiexec -np 8 --allow-run-as-root ./build/bin/LaminarPipeFlow ./apps/cpu/LaminarPipeFlow/LaminarPipeFlow.cfg"
 
 # 4. set the path to the produced data
 RESULT_DATA_DIR=output/LaminarPipeFlow
