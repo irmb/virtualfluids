e5fb388f64529da3676304c6f79832d0f3f57ae3
Soeren Peters <peters@irmb.tu-bs.de>
2023-12-05 15:21:47 +0000

    more header

--- src/gpu/core/GridScaling/GridScalingFactory.h
+++ src/gpu/core/GridScaling/GridScalingFactory.h
@@ -26,8 +26,6 @@
 //  You should have received a copy of the GNU General Public License along
 //  with VirtualFluids (see COPYING.txt). If not, see <http://www.gnu.org/licenses/>.
 //
-//! \file GridScalingFactory.h
-//! \ingroup Factories
 //! \author Anna Wellmann, Martin Schönherr
 //=======================================================================================
 #ifndef GS_FACTORY
