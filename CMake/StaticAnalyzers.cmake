5689ce44dae47c4e7d79d1d9fa2bc04cd3768661
Soeren Peters <peters@irmb.tu-bs.de>
2023-12-06 18:14:33 +0000

    Add header to config files.

--- CMake/StaticAnalyzers.cmake
+++ CMake/StaticAnalyzers.cmake
@@ -1,3 +1,31 @@
+# #######################################################################################
+# ____          ____    __    ______     __________   __      __       __        __
+# \    \       |    |  |  |  |   _   \  |___    ___| |  |    |  |     /  \      |  |
+#  \    \      |    |  |  |  |  |_)   |     |  |     |  |    |  |    /    \     |  |
+#   \    \     |    |  |  |  |   _   /      |  |     |  |    |  |   /  /\  \    |  |
+#    \    \    |    |  |  |  |  | \  \      |  |     |   \__/   |  /  ____  \   |  |____
+#     \    \   |    |  |__|  |__|  \__\     |__|      \________/  /__/    \__\  |_______|
+#      \    \  |    |   ________________________________________________________________
+#       \    \ |    |  |  ______________________________________________________________|
+#        \    \|    |  |  |         __          __     __     __     ______      _______
+#         \         |  |  |_____   |  |        |  |   |  |   |  |   |   _  \    /  _____)
+#          \        |  |   _____|  |  |        |  |   |  |   |  |   |  | \  \   \_______
+#           \       |  |  |        |  |_____   |   \_/   |   |  |   |  |_/  /    _____  |
+#            \ _____|  |__|        |________|   \_______/    |__|   |______/    (_______/
+#
+#  This file is part of VirtualFluids. VirtualFluids is free software: you can
+#  redistribute it and/or modify it under the terms of the GNU General Public
+#  License as published by the Free Software Foundation, either version 3 of
+#  the License, or (at your option) any later version.
+#
+#  VirtualFluids is distributed in the hope that it will be useful, but WITHOUT
+#  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+#  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+#  for more details.
+#
+#  You should have received a copy of the GNU General Public License along
+#  with VirtualFluids (see COPYING.txt). If not, see <http://www.gnu.org/licenses/>.
+# #######################################################################################
 macro(enable_cppcheck)
     find_program(CPPCHECK_PROGRAM cppcheck)
 
@@ -111,4 +139,3 @@ macro(enable_include_what_you_use)
     message(${WARNING_MESSAGE} "include-what-you-use requested but executable not found")
   endif()
 endmacro()
-
