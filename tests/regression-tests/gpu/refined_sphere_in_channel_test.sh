b068fa9f5adb05024a1c197c7e1214892680442a
Soeren Peters <peters@irmb.tu-bs.de>
2023-12-21 14:46:22 +0000

    Fix path for regression tests

--- tests/regression-tests/gpu/refined_sphere_in_channel_test.sh
+++ tests/regression-tests/gpu/refined_sphere_in_channel_test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # SPDX-License-Identifier: GPL-3.0-or-later
 # SPDX-FileCopyrightText: Copyright © VirtualFluids Project contributors, see AUTHORS.md in root folder
-source ./regression-tests/__regression_test_executer.sh
+source ./tests/regression-tests/__regression_test_executer.sh
 
 # 1. set reference data directory (must match the folder structure in https://github.com/irmb/test_data)
 REFERENCE_DATA_DIR=regression_tests/gpu/SphereInChannel_3Levels
