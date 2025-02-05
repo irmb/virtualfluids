f850bb246219e42f8584994656e65ad2142c25fe
Henry Korb <henry.korb@geo.uu.se>
2025-02-05 09:03:50 +0100

    add regression test for gaussian hill

---
+++ tests/regression-tests/gpu/gaussian_hill_of_concentration.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# SPDX-License-Identifier: GPL-3.0-or-later
+# SPDX-FileCopyrightText: Copyright © VirtualFluids Project contributors, see AUTHORS.md in root folder
+source ./tests/regression-tests/__regression_test_executer.sh
+
+# 1. set reference data directory (must match the folder structure in https://git.rz.tu-bs.de/irmb/virtualfluids-reference-data)
+REFERENCE_DATA_DIR=regression_tests/gpu/GaussianHillOfConcentration
+
+# 2. set cmake flags for the build of VirtualFluids
+CMAKE_FLAGS="--preset=make_gpu -DCMAKE_BUILD_TYPE=Release -DCMAKE_CUDA_ARCHITECTURES=75 -DUSER_APPS=apps/gpu/GaussianHillOfConcentration"
+
+# 3. define the application to be executed
+APPLICATION="./build/bin/GaussianHillOfConcentration ./apps/gpu/GaussianHillOfConcentration/gaussian_hill.cfg"
+
+# 4. set the path to the produced data
+RESULT_DATA_DIR=output/GaussianHillOfConcentration
+
+
+run_regression_test "$REFERENCE_DATA_DIR" "$CMAKE_FLAGS" "$APPLICATION" "$RESULT_DATA_DIR"
