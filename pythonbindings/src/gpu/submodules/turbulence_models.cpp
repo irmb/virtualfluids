f10b49bd5e28e8b0afdb064f939a608745b703f4
Henry Korb <henry.korb@geo.uu.se>
2025-02-05 16:12:13 +0100

    update python bindings

--- pythonbindings/src/gpu/submodules/turbulence_models.cpp
+++ pythonbindings/src/gpu/submodules/turbulence_models.cpp
@@ -30,7 +30,9 @@
 //=======================================================================================
 #include "pybind11/pybind11.h"
 #include "gpu/core/TurbulenceModels/TurbulenceModelFactory.h"
-#include "gpu/core/Calculation/Calculation.h"
+#include <lbm/collision/TurbulentViscosity.h>
+#include <lbm/advectionDiffusion/TurbulentDiffusivity.h>
+#include <string>
 
 namespace turbulence_model
 {
@@ -39,17 +41,13 @@ namespace turbulence_model
 
     void makeModule(py::module_ &parentModule)
     {
-        py::enum_<TurbulenceModel>(parentModule, "TurbulenceModel")
-        .value("Smagorinsky", TurbulenceModel::Smagorinsky)
-        .value("AMD", TurbulenceModel::AMD)
-        .value("QR", TurbulenceModel::QR)
-        .value("None", TurbulenceModel::None);
-
         py::class_<TurbulenceModelFactory, std::shared_ptr<TurbulenceModelFactory>>(parentModule, "TurbulenceModelFactory")
         .def(py::init< std::shared_ptr<Parameter>>(), py::arg("para"))
-        .def("set_turbulence_model", &TurbulenceModelFactory::setTurbulenceModel, py::arg("turbulence_model"))
+        .def("set_turbulence_model", static_cast<void (TurbulenceModelFactory::*)(std::string)>(&TurbulenceModelFactory::setTurbulenceModel), py::arg("turbulence_model"))
+        .def("set_turbulence_model", static_cast<void (TurbulenceModelFactory::*)(TurbulenceModel)>(&TurbulenceModelFactory::setTurbulenceModel), py::arg("turbulence_model"))
+        .def("set_turbulence_model_advection_diffusion", static_cast<void (TurbulenceModelFactory::*)(std::string)>(&TurbulenceModelFactory::setAdvectionDiffusionTurbulenceModel), py::arg("turbulence_model"))
+        .def("set_turbulence_model_advection_diffusion", static_cast<void (TurbulenceModelFactory::*)(advection_diffusion::TurbulenceModel)>(&TurbulenceModelFactory::setAdvectionDiffusionTurbulenceModel), py::arg("turbulence_model"))
         .def("set_model_constant", &TurbulenceModelFactory::setModelConstant, py::arg("model_constant"))
         .def("read_config_file", &TurbulenceModelFactory::readConfigFile, py::arg("config_data"));
-
     }
 }
