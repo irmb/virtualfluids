f35fbc54912b9570205724533abf9f3d3e89a23e
Henry Korb <henry.korb@geo.uu.se>
2025-02-14 16:54:41 +0100

    reintroduce check for small single

--- src/lbm/collision/TurbulentViscosity.h
+++ src/lbm/collision/TurbulentViscosity.h
@@ -78,8 +78,8 @@ inline __host__ __device__ real calcTurbulentViscosityQR(real C, real dxux, real
     // ! Verstappen's QR model
     //! Second invariant of the strain-rate tensor
     const real secondInvariant = c1o2 * (dxux * dxux + dyuy * dyuy + dzuz * dzuz) + c1o4 * (Dxy * Dxy + Dxz * Dxz + Dyz * Dyz);
-    // if (fabs(secondInvariant) < cSmallSingle)
-    //     return c0o1;
+    if (abs(secondInvariant) < cSmallSingle)
+        return c0o1;
     //! Third invariant of the strain-rate tensor (determinant)
     // real R = - dxux*dyuy*dzuz - c1o4*( Dxy*Dxz*Dyz + dxux*Dyz*Dyz + dyuy*Dxz*Dxz + dzuz*Dxy*Dxy );
     const real thirdInvariant = -dxux * dyuy * dzuz + c1o4 * (-Dxy * Dxz * Dyz + dxux * Dyz * Dyz + dyuy * Dxz * Dxz + dzuz * Dxy * Dxy);
