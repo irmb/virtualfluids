3ff82e2a509cb5904c2e71cd13d86a400a6cfdcb
Martin Schönherr <m.schoenherr@tu-braunschweig.de>
2025-03-28 10:59:33 +0100

    fix the real bug, it seems, that cmake 4.0 is not compatible with the older versions, so we have to exclude it for now... in the future we have to find a better fix

--- CMake/Packages.cmake
+++ CMake/Packages.cmake
@@ -122,7 +122,7 @@ FetchContent_Declare(yaml-cpp
   GIT_SHALLOW 1) 
 FetchContent_GetProperties(yaml-cpp)
 if(NOT yaml-cpp_POPULATED)
-  FetchContent_MakeAvailable(yaml-cpp)
+  FetchContent_Populate(yaml-cpp)
   add_subdirectory(${yaml-cpp_SOURCE_DIR} ${yaml-cpp_BINARY_DIR})
 endif()
 
