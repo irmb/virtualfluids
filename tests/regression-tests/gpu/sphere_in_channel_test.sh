3ae1c8f430441d9a0fc02252064d4af79de60685
Henry <henry.korb@geo.uu.se>
2025-04-16 15:10:27 +0000

    fix config in regression tests

--- tests/regression-tests/gpu/sphere_in_channel_test.sh
+++ tests/regression-tests/gpu/sphere_in_channel_test.sh
@@ -7,7 +7,7 @@ source ./tests/regression-tests/__regression_test_executer.sh
 REFERENCE_DATA_DIR=regression_tests/gpu/SphereInChannel
 
 # 2. set cmake flags for the build of VirtualFluids
-CMAKE_FLAGS="--preset=make_gpu -DCMAKE_BUILD_TYPE=Release -DCMAKE_CUDA_ARCHITECTURES=75"
+CMAKE_FLAGS="--preset=make_gpu_release -DCMAKE_CUDA_ARCHITECTURES=75"
 
 # 3. define the application to be executed
 APPLICATION="./build/bin/SphereInChannel ./apps/gpu/SphereInChannel/sphere_1level.cfg"
