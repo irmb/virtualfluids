897e3109438461a0667fb84b01bdcf9443effa99
Henry <henry.korb@geo.uu.se>
2025-05-26 08:07:04 +0000

    remove more uses of old concentration functions

--- tests/unit-tests/gpu/core/Parameter/ParameterTest.cpp
+++ tests/unit-tests/gpu/core/Parameter/ParameterTest.cpp
@@ -105,8 +105,6 @@ TEST(ParameterTest, check_all_Parameter_CanBePassedToConstructor)
 
     EXPECT_THAT(para.getDiffOn(), testing::Eq(true));
     EXPECT_THAT(para.getDiffusivity(), RealEq(1.11));
-    EXPECT_THAT(para.getConcentrationInit(), RealEq(2.22));
-    EXPECT_THAT(para.getConcentrationBC(), RealEq(3.33));
 
     EXPECT_THAT(para.getViscosity(), RealEq(4.44));
     EXPECT_THAT(para.getVelocity(), RealEq(5.55));
