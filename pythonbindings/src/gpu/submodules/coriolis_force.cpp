4ed362245f91f56d21d1fa15e20321503bcd57c4
Hkorb <henry.korb@web.de>
2025-08-15 14:29:10 +0000

    add missing python bindings

---
+++ pythonbindings/src/gpu/submodules/coriolis_force.cpp
@@ -0,0 +1,44 @@
+//=======================================================================================
+// ____          ____    __    ______     __________   __      __       __        __
+// \    \       |    |  |  |  |   _   \  |___    ___| |  |    |  |     /  \      |  |
+//  \    \      |    |  |  |  |  |_)   |     |  |     |  |    |  |    /    \     |  |
+//   \    \     |    |  |  |  |   _   /      |  |     |  |    |  |   /  /\  \    |  |
+//    \    \    |    |  |  |  |  | \  \      |  |     |   \__/   |  /  ____  \   |  |____
+//     \    \   |    |  |__|  |__|  \__\     |__|      \________/  /__/    \__\  |_______|
+//      \    \  |    |   ________________________________________________________________
+//       \    \ |    |  |  ______________________________________________________________|
+//        \    \|    |  |  |         __          __     __     __     ______      _______
+//         \         |  |  |_____   |  |        |  |   |  |   |  |   |   _  \    /  _____)
+//          \        |  |   _____|  |  |        |  |   |  |   |  |   |  | \  \   \_______
+//           \       |  |  |        |  |_____   |   \_/   |   |  |   |  |_/  /    _____  |
+//            \ _____|  |__|        |________|   \_______/    |__|   |______/    (_______/
+//
+//  This file is part of VirtualFluids. VirtualFluids is free software: you can
+//  redistribute it and/or modify it under the terms of the GNU General Public
+//  License as published by the Free Software Foundation, either version 3 of
+//  the License, or (at your option) any later version.
+//
+//  VirtualFluids is distributed in the hope that it will be useful, but WITHOUT
+//  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+//  FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+//  for more details.
+//
+//  SPDX-License-Identifier: GPL-3.0-or-later
+//  SPDX-FileCopyrightText: Copyright © VirtualFluids Project contributors, see AUTHORS.md in root folder
+//
+//! \author Henry Korb
+//=======================================================================================
+
+#include <pybind11/pybind11.h>
+#include <gpu/core/PreCollisionInteractor/CoriolisForce.h>
+
+namespace coriolis_force
+{
+    void makeModule(py::module_ &parentModule)
+    {
+        
+        py::class_<CoriolisForce, PreCollisionInteractor, std::shared_ptr<CoriolisForce>>(parentModule, "CoriolisForce")
+        .def(py::init<SPtr<Parameter>, SPtr<CudaMemoryManager>, real, real, real>,
+             py::arg("para"), py::arg("cuda_memory_manager"), py::arg("geostrophic_wind_x"), py::arg("geostrophic_wind_y"), py::arg("coriolis_parameter"));
+    }
+}
